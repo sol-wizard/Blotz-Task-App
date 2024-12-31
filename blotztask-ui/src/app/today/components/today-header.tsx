3ed43f724c51289c11fadd3f917525a40140b07a
Sean <xhma0211@gmail.com>
2024-12-31 22:22:05 +1100

    add date at the today heading (#167)

--- blotztask-ui/src/app/today/components/today-header.tsx
+++ blotztask-ui/src/app/today/components/today-header.tsx
@@ -1,12 +1,16 @@
 import { H1, H5 } from "@/components/ui/heading-with-anchor";
 import React from "react";
+import { format } from "date-fns";
 
 const TodayHeader = () => {
+  const todayDate = format(new Date(), "EEEE, d MMMM");
+
   return (
     <div className="flex flex-col gap-5">
       <H1 className="heading-primary">
         To<span className="heading-secondary">day</span>
       </H1>
+      <H5 className="text-gray-500 text-sm">{todayDate}</H5>
       <H5>List of today&apos;s tasks</H5>
     </div>
   );
