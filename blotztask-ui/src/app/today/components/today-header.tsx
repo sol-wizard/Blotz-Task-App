9c0e064c2edcabd4d851080c3e0e69f7f3d9ecf5
Rachel <43347454+RachelYrt@users.noreply.github.com>
2025-01-01 19:38:28 +0800

    Add a progress bar (#166)
    Co-authored-by: benjaminneoh <benjaminneoh6343@gmail.com>

--- blotztask-ui/src/app/today/components/today-header.tsx
+++ blotztask-ui/src/app/today/components/today-header.tsx
@@ -1,16 +1,33 @@
-import { H1, H5 } from "@/components/ui/heading-with-anchor";
-import React from "react";
-import { format } from "date-fns";
+import { H1, H5 } from '@/components/ui/heading-with-anchor';
+import React from 'react';
+import { Progress } from '@/components/ui/progress';
+import { TaskDTO } from '../schema/schema';
 
-const TodayHeader = () => {
-  const todayDate = format(new Date(), "EEEE, d MMMM");
+type TodayHeaderProps = {
+  tasks: TaskDTO[]; // All tasks
+};
+
+const TodayHeader: React.FC<TodayHeaderProps> = ({ tasks }) => {
+  const total = tasks.length; // Total number of tasks
+  const completed = tasks.filter((task) => task.isDone).length; // Number of completed tasks
+  const progressValue = total > 0 ? (completed / total) * 100 : 0; // Avoid division by zero
 
   return (
     <div className="flex flex-col gap-5">
-      <H1 className="heading-primary">
-        To<span className="heading-secondary">day</span>
-      </H1>
-      <H5 className="text-gray-500 text-sm">{todayDate}</H5>
+      <div className="flex justify-between items-center">
+        <H1 className="heading-primary flex items-center">
+          Day<span className="heading-secondary">View</span>
+        </H1>
+        <div className="flex items-center gap-2">
+          {/* Label for progress bar */}
+          <span className="text-sm font-medium">Completed</span>
+          <Progress value={progressValue} className="w-[200px]" />
+          {/* Label for task progress */}
+          <span className="text-gray-500 text-sm font-medium">
+            {completed} / {total}
+          </span>
+        </div>
+      </div>
       <H5>List of today&apos;s tasks</H5>
     </div>
   );
