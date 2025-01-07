9abe5d06eafcfa16199b43b16b34f1705682ce68
nxn-nicole <nxnnxn2@163.com>
2025-01-07 21:13:21 +1100

    refactor the task card component (#164)
    Co-authored-by: benjaminneoh <benjaminneoh6343@gmail.com>

--- blotztask-ui/src/app/today/schema/schema.ts
+++ blotztask-ui/src/app/today/schema/schema.ts
@@ -1,22 +1,10 @@
 'use client';
 
-import { z } from 'zod';
+import { LabelDTO } from '@/model/label-dto';
 
-export const formSchema = z.object({
-  title: z.string().min(1, {
-    message: 'Title is required',
-  }),
-  description: z.string(),
-});
-
-// this is the schema we want for our application
-export const taskDTOSchema = z.object({
-  id: z.number(),
-  title: z.string(),
-  description: z.string(),
-  isDone: z.boolean(),
-  createdAt: z.string().transform((str) => new Date(str)), // Convert string to Date
-  updatedAt: z.string().transform((str) => new Date(str)), // Convert string to Date
-});
-
-export type TaskDTO = z.infer<typeof taskDTOSchema>;
+export interface TaskDTO {
+  id: number;
+  title: string;
+  isDone: boolean;
+  label: LabelDTO;
+}
