6abeac289df97c8aeb4328627dd57d348a0d03c2
xiaoyu <86012424+insidethewall@users.noreply.github.com>
2025-01-11 21:12:25 +1000

    Issue 316 card component not align (#177)
    Co-authored-by: benjaminneoh <benjaminneoh6343@gmail.com>

---
+++ blotztask-ui/src/app/today/components/task-content.tsx
@@ -0,0 +1,28 @@
+import { TaskDTO } from '../schema/schema';
+
+export default function TaskContent({ task }: { task: TaskDTO }) {
+  return (
+    <div
+      className="flex flex-col w-full bg-transparent px-6 border-l-4 border-primary">
+      <div>
+        <p className="font-bold">{task?.title}</p>
+      </div>
+
+      <div className="flex w-full text-sm text-gray-500 mt-2">
+        <div className="flex flex-col w-full">
+          <p>{task?.description}</p>
+        </div>
+
+        <div className="flex items-start ml-4 w-32">
+          <div
+            className="h-4 w-4 rounded-full"
+            style={{ backgroundColor: task.label.color || 'gray' }}
+          ></div>
+          <span className="ml-2 font-bold">
+            {task.label?.name || 'No label name'}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
