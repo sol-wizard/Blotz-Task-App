1234bffd3aefc6bfdcc71de99cd8a201e2ff418c
nxn-nicole <nxnnxn2@163.com>
2025-01-13 18:51:42 +1100

    Add due day to today's page (#179)

--- blotztask-ui/src/app/today/components/task-content.tsx
+++ blotztask-ui/src/app/today/components/task-content.tsx
@@ -1,11 +1,12 @@
 import { TaskDTO } from '../schema/schema';
+import DueDayTag from './due-day-tag';
 
 export default function TaskContent({ task }: { task: TaskDTO }) {
   return (
-    <div
-      className="flex flex-col w-full bg-transparent px-6 border-l-4 border-primary">
-      <div>
+    <div className="flex flex-col w-full bg-transparent px-6 border-l-4 border-primary">
+      <div className="flex flex-row justify-between w-full">
         <p className="font-bold">{task?.title}</p>
+        <DueDayTag task={task} />
       </div>
 
       <div className="flex w-full text-sm text-gray-500 mt-2">
