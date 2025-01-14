5eaec5217343f0e6eb8efae5869adf44806cb21b
nxn-nicole <nxnnxn2@163.com>
2025-01-14 18:34:46 +1100

    Today - Add Hover Buttons to Task Card (#183)

--- blotztask-ui/src/app/today/components/task-content.tsx
+++ blotztask-ui/src/app/today/components/task-content.tsx
@@ -1,10 +1,11 @@
 import { TaskDetailDTO } from '../../task-list/models/task-detail-dto';
 import DueDateTag from './due-date-tag';
 import TaskSeparator from '../shared/task-separator';
+import { Pencil, Trash2 } from 'lucide-react';
 
 export default function TaskContent({ task }: { task: TaskDetailDTO }) {
   return (
-    <div className="flex flex-row w-full bg-transparent">
+    <div className="flex flex-row w-full bg-transparent group">
       <TaskSeparator color={task.label.color} />
 
       <div className="flex flex-col w-full bg-transparent px-6">
@@ -18,12 +19,21 @@ export default function TaskContent({ task }: { task: TaskDetailDTO }) {
             <p>{task?.description}</p>
           </div>
 
-          <div className="flex items-start ml-4 w-32">
+          <div className="flex items-start ml-4 w-32 group-hover:hidden">
             <div
               className="h-4 w-4 rounded-full"
               style={{ backgroundColor: task.label.color || 'gray' }}
             ></div>
             <span className="ml-2 font-bold">{task.label?.name || 'No label name'}</span>
+          </div>
+
+          <div className="flex justify-end hidden ml-4 w-32 group-hover:flex">
+            <button className="px-4">
+              <Pencil className="text-primary" size={20} />
+            </button>
+            <button>
+              <Trash2 className="text-primary" size={20} />
+            </button>
           </div>
         </div>
       </div>
