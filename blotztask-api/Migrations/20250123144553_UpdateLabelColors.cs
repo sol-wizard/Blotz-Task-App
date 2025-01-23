c0a280568769e185e5b7d05ba45bca2ca0d0246a
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-01-24 03:06:29 +1100

    Change default label color ef migration (#190)
    Change default label color.

---
+++ blotztask-api/Migrations/20250123144553_UpdateLabelColors.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlotzTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLabelColors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 6,
+                column: "Color",
+                value: "#7758FF");
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 7,
+                column: "Color",
+                value: "#FFDE23");
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 8,
+                column: "Color",
+                value: "#FF4747");
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 9,
+                column: "Color",
+                value: "#09F1D6");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 6,
+                column: "Color",
+                value: "#CDB2FF");
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 7,
+                column: "Color",
+                value: "#FBFAC2");
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 8,
+                column: "Color",
+                value: "#278291");
+
+            migrationBuilder.UpdateData(
+                table: "Labels",
+                keyColumn: "LabelId",
+                keyValue: 9,
+                column: "Color",
+                value: "#1458C6");
+        }
+    }
+}
