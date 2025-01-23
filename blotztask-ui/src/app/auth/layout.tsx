b9a77e70d7e7c5f89ba8ee58294feb6098a27b9b
Nicole <nicolenxn1234@gmail.com>
2025-01-23 17:20:02 +0800

    Add login button navigation (#189)

--- blotztask-ui/src/app/auth/layout.tsx
+++ blotztask-ui/src/app/auth/layout.tsx
@@ -1,10 +1,11 @@
 import BrandingSection from "./components/branding-section";
+import AuthButton from "./components/auth-button";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex container relative h-screen flex-rows items-center justify-center lg:max-w-none lg:px-0">
       <BrandingSection />
-
+      <AuthButton />
       <div className="flex-[3] lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           {children}
