928b68b011503853045a4e215a28e22cff04d977
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-01-29 12:53:37 +1100

    Get current login user info (#192)
    Co-authored-by: Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>

---
+++ blotztask-api/Models/UserInfoDTO.cs
@@ -0,0 +1,10 @@
+namespace BlotzTask.Models
+{
+    public class UserInfoDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email {get; set;} = string.Empty;
+
+        public string Message {get; set;} = string.Empty;
+    }
+}
