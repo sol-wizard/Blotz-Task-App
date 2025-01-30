94413a94fc2b30cb93f4d751021ce7fbb369eeac
nxn-nicole <nxnnxn2@163.com>
2025-01-30 17:47:35 +1100

    Task card edit form UI 2 (#194)
    Co-authored-by: benjaminneoh <benjaminneoh6343@gmail.com>

--- blotztask-ui/src/app/dashboard/today/components/task-card.tsx
+++ blotztask-ui/src/app/dashboard/today/components/task-card.tsx
@@ -4,15 +4,17 @@ import TaskContent from './task-content';
 
 const TaskCard = ({ task, handleCheckboxChange }) => {
   return (
-    <div className="flex w-full">
-      <div className="flex justify-start items-center">
-        <Checkbox
-          checked={task.isDone}
-          onCheckedChange={() => handleCheckboxChange(task.id)}
-          className="h-6 w-6 mr-6 rounded-full border-2 border-black"
-        />
+    <div>
+      <div className="flex w-full">
+        <div className="flex justify-start items-center">
+          <Checkbox
+            checked={task.isDone}
+            onCheckedChange={() => handleCheckboxChange(task.id)}
+            className="h-6 w-6 mr-6 rounded-full border-2 border-black"
+          />
+        </div>
+        <TaskContent task={task} />
       </div>
-      <TaskContent task={task} />
     </div>
   );
 };
