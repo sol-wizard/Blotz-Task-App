3d28543c19969f97e78ebaccec8f3650d6c8b604
nxn-nicole <nxnnxn2@163.com>
2025-01-30 23:50:08 +1100

    Add task refactor form component (#198)

--- blotztask-ui/src/app/dashboard/task-list/components/task-list.tsx
+++ blotztask-ui/src/app/dashboard/task-list/components/task-list.tsx
@@ -1,10 +1,10 @@
-import TaskCard from "../../today/components/task-card";
+import TaskCard from '../../today/components/task-card';
 
 export function TaskList({ tasks, handleCheckboxChange }) {
   return (
     <div className="flex flex-col mt-10 w-full">
       {tasks.map((task) => (
-        <div key={task.id} className="flex mt-5">
+        <div key={task.id} className="w-full mt-5">
           <TaskCard task={task} handleCheckboxChange={handleCheckboxChange}></TaskCard>
         </div>
       ))}
