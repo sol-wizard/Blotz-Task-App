d71bdaf5ee57594dae956e5a2abf6ac70ff65284
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-02-01 23:43:30 +1100

    Fix bug click away (#199)

--- blotztask-ui/src/app/dashboard/today/components/add-task-card.tsx
+++ blotztask-ui/src/app/dashboard/today/components/add-task-card.tsx
@@ -1,18 +1,20 @@
 import React, { useState, useRef } from 'react';
 import AddTaskForm from './add-task-form';
 import { PlusIcon } from '@radix-ui/react-icons';
-import { useClickAway } from 'react-use';
+import  useClickOutside from '@/utils/use-multiple-click-away';
 
 const AddTaskCard = ({ onAddTask }) => {
   const [isFormVisible, setIsFormVisible] = useState(false);
-  const formRef = useRef(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const datePickerRef = useRef<HTMLDivElement>(null);
+  const labelPickerRef = useRef<HTMLDivElement>(null);
 
-  useClickAway(formRef, () => {
+  useClickOutside([cardRef, datePickerRef, labelPickerRef], () => {
     setIsFormVisible(false);
   });
 
   return (
-    <div className="flex w-full items-center gap-4">
+    <div className="flex w-full items-center gap-4" ref = {cardRef}>
       <div className="w-6 h-6 border border-gray-400 rounded-full border-dashed"></div>
       <div className="ml-3 w-1 bg-gray-400 rounded"></div>
       <div className="flex w-full items-center gap-2 cursor-pointer" onClick={() => setIsFormVisible(true)}>
@@ -22,14 +24,14 @@ const AddTaskCard = ({ onAddTask }) => {
             <span className="text-blue-400 font-semibold text-lg">Add a task</span>
           </>
         ) : (
-          <div className="flex w-full" ref={formRef}>
-            <AddTaskForm
-              onSubmit={(taskTitle: string) => {
-                onAddTask(taskTitle);
-                setIsFormVisible(false);
-              }}
-            />
-          </div>
+          <AddTaskForm
+            onSubmit={(taskTitle: string) => {
+              onAddTask(taskTitle);
+              setIsFormVisible(false);
+            }}
+            datePickerRef={datePickerRef}
+            labelPickerRef={labelPickerRef}
+          />
         )}
       </div>
     </div>
