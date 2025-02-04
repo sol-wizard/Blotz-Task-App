77fb8a13eecb9448f9b0423472a0147824682794
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-02-04 14:43:53 +1100

    Updated separator styling (#202)

--- blotztask-ui/src/app/dashboard/today/components/divider.tsx
+++ blotztask-ui/src/app/dashboard/today/components/divider.tsx
@@ -1,15 +1,15 @@
-import SectionSepreator from './section-seperator';
+import SectionSeparator from './section-separator';
 
 interface DividerProps {
   text:string;
 }
 const Divider = ({text}:DividerProps) => {
   return (
-    <div className="flex items-center space-x-7">
-      <p className="font-hind font-semibold text-[40px] leading-[48px] text-primary-dark -tracking-[0.41]">
+    <div className="flex flex-col space-y-4">
+      <p className="font-hind font-semibold text-[40px] leading-[48px] text-primary-dark -tracking-[0.41px]">
         {text}
       </p>
-      <SectionSepreator />
+      <SectionSeparator />
     </div>
   );
 };
--- blotztask-ui/src/app/dashboard/today/components/section-seperator.tsx
+++
@@ -1,7 +0,0 @@
-const SectionSepreator = () => {
-  return (
-    <div className="flex-1 border-t border-[1.5px]  border-[#808484] opacity-25"></div>
-  );
-};
-
-export default SectionSepreator;
