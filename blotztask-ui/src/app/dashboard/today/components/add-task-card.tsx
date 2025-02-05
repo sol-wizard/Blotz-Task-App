836835ecc9e89fda7c2450f1abe7793bfd9ec45b
nxn-nicole <nxnnxn2@163.com>
2025-02-05 11:41:31 +1100

    add task form with react hook (#201)
    Co-authored-by: benjaminneoh <benjaminneoh6343@gmail.com>

--- blotztask-ui/src/app/dashboard/today/components/add-task-card.tsx
+++ blotztask-ui/src/app/dashboard/today/components/add-task-card.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import AddTaskForm from './add-task-form';
 import { PlusIcon } from '@radix-ui/react-icons';
-import  useClickOutside from '@/utils/use-multiple-click-away';
+import useClickOutside from '@/utils/use-multiple-click-away';
 
 const AddTaskCard = ({ onAddTask }) => {
   const [isFormVisible, setIsFormVisible] = useState(false);
@@ -14,15 +14,15 @@ const AddTaskCard = ({ onAddTask }) => {
   });
 
   return (
-    <div className="flex w-full items-center gap-4" ref = {cardRef}>
+    <div className="flex w-full items-center gap-4" ref={cardRef}>
       <div className="w-6 h-6 border border-gray-400 rounded-full border-dashed"></div>
       <div className="ml-3 w-1 bg-gray-400 rounded"></div>
-      <div className="flex w-full items-center gap-2 cursor-pointer" onClick={() => setIsFormVisible(true)}>
+      <div className="flex w-full items-center gap-2 cursor-pointer">
         {!isFormVisible ? (
-          <>
+          <div className="flex flex-row" onClick={() => setIsFormVisible(true)}>
             <PlusIcon className="w-6 h-6 text-blue-400" />
             <span className="text-blue-400 font-semibold text-lg">Add a task</span>
-          </>
+          </div>
         ) : (
           <AddTaskForm
             onSubmit={(taskTitle: string) => {
@@ -31,6 +31,7 @@ const AddTaskCard = ({ onAddTask }) => {
             }}
             datePickerRef={datePickerRef}
             labelPickerRef={labelPickerRef}
+            onCancel={() => setIsFormVisible(false)}
           />
         )}
       </div>
