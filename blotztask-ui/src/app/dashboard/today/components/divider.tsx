c22541d631b14dc00dc127261b4f5f88fe3cf801
Emilia-Z <keirinzeng@gmail.com>
2025-02-08 00:26:47 +1100

    font size change  (#210)

--- blotztask-ui/src/app/dashboard/today/components/divider.tsx
+++ blotztask-ui/src/app/dashboard/today/components/divider.tsx
@@ -1,16 +1,14 @@
-import SectionSeparator from './section-separator';
-
 interface DividerProps {
   text:string;
 }
 const Divider = ({text}:DividerProps) => {
   return (
-    <div className="flex flex-col space-y-4">
-      <p className="font-hind font-semibold text-[40px] leading-[48px] text-primary-dark -tracking-[0.41px]">
-        {text}
-      </p>
-      <SectionSeparator />
-    </div>
+
+  <div className="flex items-center space-x-7">
+  <p className="font-hind font-semibold text-[25px] leading-[30px] text-primary-dark -tracking-[0.41]">
+  {text}
+</p>
+</div>
   );
 };
 
