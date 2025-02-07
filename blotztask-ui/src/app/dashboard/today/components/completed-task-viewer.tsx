6ece2b0cccd0290eb8415cdb2586d346a3fbbfe7
Alexis <alexissong07@gmail.com>
2025-02-07 14:02:41 +1100

    font size change and delete sentence

--- blotztask-ui/src/app/dashboard/today/components/completed-task-viewer.tsx
+++ blotztask-ui/src/app/dashboard/today/components/completed-task-viewer.tsx
@@ -7,7 +7,6 @@ export function CompletedTaskViewer({
 }) {
   return (
     <div>
-      <H5>Completed tasks</H5>
       <div className="grid gap-6 w-full">
         {completedTasks.length > 0 ? (
           <div className="grid gap-6 w-full">
--- blotztask-ui/src/app/dashboard/today/components/divider.tsx
+++ blotztask-ui/src/app/dashboard/today/components/divider.tsx
@@ -6,7 +6,7 @@ interface DividerProps {
 const Divider = ({text}:DividerProps) => {
   return (
     <div className="flex items-center space-x-7">
-      <p className="font-hind font-semibold text-[40px] leading-[48px] text-primary-dark -tracking-[0.41]">
+      <p className="font-hind font-semibold text-[25px] leading-[30px] text-primary-dark -tracking-[0.41]">
         {text}
       </p>
       <SectionSepreator />
