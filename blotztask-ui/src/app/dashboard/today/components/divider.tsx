3c4dbc995d83dacbac279e3f738465592bbd47bd
Alexis <alexissong07@gmail.com>
2025-02-07 14:23:49 +1100

    fix bug

--- blotztask-ui/src/app/dashboard/today/components/divider.tsx
+++ blotztask-ui/src/app/dashboard/today/components/divider.tsx
@@ -7,7 +7,7 @@ const Divider = ({text}:DividerProps) => {
   return (
     <div className="flex items-center space-x-7">
       <p className="font-hind font-semibold text-[25px] leading-[30px] text-primary-dark -tracking-[0.41]">
-        {text}
+         {text}
       </p>
       <SectionSepreator />
     </div>
