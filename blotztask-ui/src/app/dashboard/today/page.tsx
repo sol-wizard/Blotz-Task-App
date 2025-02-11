bee9a1cf1e7659445f1a7d2bb92c4773cfba123a
cindyTAT <wanchengt@student.unimelb.edu.au>
2025-02-11 21:47:38 +1100

    Changing page margin

--- blotztask-ui/src/app/dashboard/today/page.tsx
+++ blotztask-ui/src/app/dashboard/today/page.tsx
@@ -62,24 +62,26 @@ export default function Today() {
 
   return (
     <>
-      <div className="flex flex-col gap-6">
+      <div className="ml-5 flex flex-col gap-12">
         <TodayHeader tasks={tasks} />
-        <AddTaskCard onAddTask={(newTaskData) => handleAddTask(newTaskData)} />
-        <Divider text="To do" />
-        <div className="flex flex-col gap-6 w-full">
-          {incompleteTasks.length > 0 ? (
-            incompleteTasks.map((task) => (
-              <TaskCard key={task.id} task={task} handleCheckboxChange={handleCheckboxChange}></TaskCard>
-            ))
-          ) : (
-            <p>No incomplete tasks for today!</p>
-          )}
+        <div className="flex flex-col gap-6">
+          <AddTaskCard onAddTask={(newTaskData) => handleAddTask(newTaskData)} />
+          <Divider text="To do" />
+          <div className="flex flex-col gap-6 w-full">
+            {incompleteTasks.length > 0 ? (
+              incompleteTasks.map((task) => (
+                <TaskCard key={task.id} task={task} handleCheckboxChange={handleCheckboxChange}></TaskCard>
+              ))
+            ) : (
+              <p>No incomplete tasks for today!</p>
+            )}
+          </div>
+          <Divider text="Done" />
+          <CompletedTaskViewer
+            completedTasks={completedTasks}
+            handleCompletedCheckboxChange={handleCompletedCheckboxChange}
+          />
         </div>
-        <Divider text="Done" />
-        <CompletedTaskViewer
-          completedTasks={completedTasks}
-          handleCompletedCheckboxChange={handleCompletedCheckboxChange}
-        />
       </div>
     </>
   );
