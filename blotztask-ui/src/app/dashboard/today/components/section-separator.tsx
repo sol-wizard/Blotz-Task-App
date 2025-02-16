fc6a04db2fdca703cccfb692d89a340b1e48d679
Emilia-Z <keirinzeng@gmail.com>
2025-02-16 15:52:00 +1100

    Ensure content centrally (#215)

--- blotztask-ui/src/app/dashboard/today/components/section-separator.tsx
+++ blotztask-ui/src/app/dashboard/today/components/section-separator.tsx
@@ -1,6 +1,6 @@
 const SectionSeparator = () => {
   return (
-    <div className="flex-1 border-t border-[1.5px]  border-primary opacity-50"></div>
+    <div className="flex-1 border-t border-[1.5px]  border-primary opacity-50 mt-2"></div>
   );
 };
 
