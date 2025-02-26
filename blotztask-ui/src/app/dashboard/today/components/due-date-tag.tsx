75f3fc730c6091ce9e070be045f1ef50e9a59e4e
Nicole NXN <nicolenxn1234@gmail.com>
2025-02-27 10:30:32 +1100

    update duedate color

--- blotztask-ui/src/app/dashboard/today/components/due-date-tag.tsx
+++ blotztask-ui/src/app/dashboard/today/components/due-date-tag.tsx
@@ -5,8 +5,8 @@ const DueDateTag = ({ task }: { task: TaskDetailDTO }) => {
   return (
     <div
       className="flex items-center justify-center w-40 
-                 text-xs text-gray-500 rounded-full"
-      style={{ background: task.isDone ? undefined : '#E5E7EB' }}
+                 text-xs rounded-full"
+      style={{ background: task.isDone ? undefined : '#E5E7EB', color: task.isDone ? '#BFC0C9' : '#6B7280' }}
     >
       Due day - {format(new Date(task.dueDate), 'MM/dd/yyyy')}
     </div>
