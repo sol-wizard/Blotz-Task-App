740f7d0d49645a00558f48f7ef4912672ff83f96
Nicole <nicolenxn1234@gmail.com>
2025-02-28 13:21:45 +1100

    Task completion toggle with visual feedback (#225)

--- blotztask-ui/src/app/dashboard/today/shared/task-separator.tsx
+++ blotztask-ui/src/app/dashboard/today/shared/task-separator.tsx
@@ -1,12 +1,12 @@
 import { Separator } from '@/components/ui/separator';
 
-const TaskSeparator = (props) => {
+const TaskSeparator = ({ color, isDone }) => {
   return (
     <Separator
       orientation="vertical"
       decorative={true}
-      className="w-[4px]"
-      style={{ background: props.color || '#9698A7' }}
+      className="w-[4px] rounded-md"
+      style={{ background: isDone ? '#BFC0C9' : color || '#9698A7' }}
     />
   );
 };
