9499188bae1feb096f36108408a0675b105afd4a
Nicole NXN <nicolenxn1234@gmail.com>
2025-03-06 11:42:41 +1100

    add seperator

--- blotztask-ui/src/app/dashboard/navbar/components/global-add-task-form.tsx
+++ blotztask-ui/src/app/dashboard/navbar/components/global-add-task-form.tsx
@@ -7,6 +7,9 @@ import { AddTaskItemDTO } from '@/model/add-task-item-dto';
 import { format } from 'date-fns';
 import { taskFormSchema } from '../../today/forms/task-form-schema';
 import AddTaskFormField from '../../today/components/add-task-form-field';
+import { Separator } from '@/components/ui/separator';
+import { TrendingUp } from 'lucide-react';
+import TaskSeparator from '../../today/shared/task-separator';
 
 type FormField = z.infer<typeof taskFormSchema>;
 
@@ -34,9 +37,14 @@ const GlobalAddTaskForm = ({ onSubmit, datePickerRef, labelPickerRef, onCancel }
 
   return (
     <Form {...form}>
-      <form className="flex flex-col w-full space-y-2" onSubmit={form.handleSubmit(handleAddTask)}>
-        <AddTaskFormField form={form} datePickerRef={datePickerRef} labelPickerRef={labelPickerRef} />
-        <div className="flex flex-row justify-end">
+      <form className="flex flex-col space-y-2" onSubmit={form.handleSubmit(handleAddTask)}>
+        <div className="flex flex-row justify-center mb-3">
+          <div className="w-6 h-6 mt-8 mr-4 border-2 border-gray-400 rounded-full border-dashed"></div>
+          <TaskSeparator color="#c7d2fe" className="mx-4" />
+          <AddTaskFormField form={form} datePickerRef={datePickerRef} labelPickerRef={labelPickerRef} />
+        </div>
+        <Separator className="bg-indigo-200" />
+        <div className="flex flex-row justify-end mt-4">
           <div className="flex flex-row h-8 ml-4">
             <button
               className="bg-neutral-300 rounded-lg px-3 py-2 text-xs text-gray-700 mx-2 w-20"
