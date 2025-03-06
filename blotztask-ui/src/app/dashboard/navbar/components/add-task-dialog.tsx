4adbad7de0f18618bd99dc878a573fa55e3c01a3
Nicole NXN <nicolenxn1234@gmail.com>
2025-03-06 12:54:08 +1100

    delete useless import

--- blotztask-ui/src/app/dashboard/navbar/components/add-task-dialog.tsx
+++ blotztask-ui/src/app/dashboard/navbar/components/add-task-dialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, createContext, useContext, useRef } from 'react';
+import { useState, createContext, useContext } from 'react';
 import { AlertDialog, AlertDialogContent } from '@/components/ui/alert-dialog';
 import GlobalAddTaskForm from './global-add-task-form';
 import { addTaskItem } from '@/services/taskService';
