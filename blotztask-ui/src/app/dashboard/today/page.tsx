e4a482f4c9caa2c980590785545e4e594ce9c6d8
Nicole <nicolenxn1234@gmail.com>
2025-03-08 18:22:46 +1100

    Initial setup state management (#241)

--- blotztask-ui/src/app/dashboard/today/page.tsx
+++ blotztask-ui/src/app/dashboard/today/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import {
   addTaskItem,
   deleteTask,
   editTask,
-  fetchTaskItemsDueToday,
   undoDeleteTask,
 } from '@/services/taskService';
 import { updateTaskStatus } from '@/services/taskService';
@@ -14,34 +13,17 @@ import TaskCard from './components/task-card';
 import AddTaskCard from './components/add-task-card';
 import { CompletedTaskViewer } from './components/completed-task-viewer';
 import Divider from './components/divider';
-import { TaskDetailDTO } from '@/app/dashboard/task-list/models/task-detail-dto';
 import { AddTaskItemDTO } from '@/model/add-task-item-dto';
 import LoadingSpinner from '../../../components/ui/loading-spinner';
 import { EditTaskItemDTO } from '../task-list/models/edit-task-item-dto';
+import { useTodayTaskStore } from '../store/today-task-store';
 
 export default function Today() {
-  const [tasks, setTasks] = useState<TaskDetailDTO[]>([]); // Store all tasks here
-  const [incompleteTasks, setIncompleteTasks] = useState<TaskDetailDTO[]>([]);
-  const [completedTasks, setCompletedTasks] = useState<TaskDetailDTO[]>([]);
-  const [loading, setLoading] = useState(false);
+  const { todayTasks, incompleteTodayTasks, completedTodayTasks, todayTasksIsLoading, loadTasks, setLoading } = useTodayTaskStore();
 
   useEffect(() => {
     loadTasks();
   }, []);
-
-  const loadTasks = async () => {
-    try {
-      const data = await fetchTaskItemsDueToday();
-      setTasks(data);
-      // Filter tasks to only include those where isDone is false
-      const notDoneTasks = data.filter((task) => !task.isDone);
-      setIncompleteTasks(notDoneTasks);
-      const doneTask = data.filter((task) => task.isDone);
-      setCompletedTasks(doneTask);
-    } catch (error) {
-      console.error('Error loading tasks:', error);
-    }
-  };
 
   /** Helper function to handle API action ensure consistent behaviour and avoid duplicate code */
   const handleAction = async (action: () => Promise<unknown>) => {
@@ -80,7 +62,7 @@ export default function Today() {
     <>
       <div className="ml-5 flex flex-col gap-12">
         <div className="flex flex-col gap-6">
-          {loading ? (
+          {todayTasksIsLoading ? (
             <div className="flex justify-center items-center min-h-screen">
               <div>
                 <LoadingSpinner variant="blue" className="mb-12 ml-8 text-[10px]" />
@@ -89,12 +71,12 @@ export default function Today() {
             </div>
           ) : (
             <>
-              <TodayHeader tasks={tasks} />
+              <TodayHeader tasks={todayTasks} />
               <AddTaskCard onAddTask={(newTaskData) => handleAddTask(newTaskData)} />
               <Divider text="To do" />
               <div className="flex flex-col gap-6 w-full">
-                {incompleteTasks.length > 0 ? (
-                  incompleteTasks.map((task) => (
+                {incompleteTodayTasks.length > 0 ? (
+                  incompleteTodayTasks.map((task) => (
                     <TaskCard
                       key={task.id}
                       task={task}
@@ -110,7 +92,7 @@ export default function Today() {
               </div>
               <Divider text="Done" />
               <CompletedTaskViewer
-                completedTasks={completedTasks}
+                completedTasks={completedTodayTasks}
                 handleCompletedCheckboxChange={handleCheckboxChange}
                 handleTaskEdit={handleTaskEdit}
                 handleTaskDelete={handleTaskDelete}
