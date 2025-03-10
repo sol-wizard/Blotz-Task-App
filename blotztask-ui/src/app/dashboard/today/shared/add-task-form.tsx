adfa49c4b7d5697ff1f5db7156238da276b44296
Nicole <nicolenxn1234@gmail.com>
2025-03-10 22:05:42 +1100

    Add task globally from side nav (#238)
    Co-authored-by: benjaminneoh <benjaminneoh6343@gmail.com>

---
+++ blotztask-ui/src/app/dashboard/today/shared/add-task-form.tsx
@@ -0,0 +1,52 @@
+import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { CalendarForm } from './calendar-form';
+import { LabelSelect } from './label-select';
+import { Input } from '@/components/ui/task-card-input';
+import { Textarea } from '@/components/ui/textarea';
+
+const AddTaskForm = ({
+  form,
+  datePickerRef,
+  labelPickerRef,
+}: {
+  form;
+  datePickerRef?: React.RefObject<HTMLDivElement>;
+  labelPickerRef?: React.RefObject<HTMLDivElement>;
+}) => {
+  return (
+    <div className="flex flex-col w-full">
+      <FormField
+        control={form.control}
+        name="title"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input placeholder="Enter task title" className="font-bold text-base" {...field}></Input>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Textarea placeholder="Fill in the detailed information" className="w-full" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <div className="flex flex-row space-x-4 items-center flex-1">
+        <div className="flex flex-row items-center">
+          <CalendarForm control={form.control} datePickerRef={datePickerRef} />
+          <LabelSelect control={form.control} labelPickerRef={labelPickerRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTaskForm;
