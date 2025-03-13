b989d43e7bc50aad8bad22725e172a22800f8955
Rachel <43347454+RachelYrt@users.noreply.github.com>
2025-03-13 21:38:01 +1100

    register endpoint (#246)
    Co-authored-by: Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>

---
+++ blotztask-api/Models/RegisterRequestDTO.cs
@@ -0,0 +1,11 @@
+namespace BlotzTask.Models
+{
+    public class RegisterRequestDTO
+    {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    }
+
+}
