93fab22a6ea1084c43d59d26437ee65afd9dc461
Nicole NXN <nicolenxn1234@gmail.com>
2025-03-15 09:54:12 +1100

    implement choose task function

--- blotztask-ui/src/app/dashboard/navbar/components/search-bar.tsx
+++ blotztask-ui/src/app/dashboard/navbar/components/search-bar.tsx
@@ -1,28 +1,15 @@
 import { Command, CommandInput } from '@/components/ui/command';
 import useClickOutside from '@/utils/use-multiple-click-away';
 import { CommandEmpty, CommandItem, CommandList } from 'cmdk';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useSearchTaskStore } from '../../store/search-task-store';
-const mockTasks = [
-  { id: 1, title: 'Complete Assignment 1', isDone: true },
-  { id: 2, title: 'Review Lecture Notes', isDone: false },
-  { id: 3, title: 'Attend Study Group', isDone: true },
-  { id: 4, title: 'Complete Assignment 2', isDone: true },
-];
-
-const filterTasks = (query) => {
-  if (query.length > 2) {
-    return mockTasks.filter((task) => task.title.toLowerCase().includes(query.toLowerCase()));
-  }
-  return [];
-};
 
 const SearchBar = () => {
-  const { query, setQuery } = useSearchTaskStore();
-  const filteredTasks = filterTasks(query);
+  const { query, setQuery, filteredTasks, searchPageRef, filterTasks, selectTasks } = useSearchTaskStore();
+
   const commandRef = useRef<HTMLDivElement>(null);
 
-  useClickOutside([commandRef], () => {
+  useClickOutside([commandRef, searchPageRef], () => {
     setQuery('');
   });
 
@@ -31,17 +18,25 @@ const SearchBar = () => {
       className={`rounded-xl mt-4 max-h-60 border ${query ? 'border-gray-300' : 'border-transparent'}`}
       ref={commandRef}
     >
-      <CommandInput placeholder="Search a task..." value={query} onValueChange={setQuery} />
+      <CommandInput
+        placeholder="Search a task..."
+        value={query}
+        onValueChange={(value) => {
+          setQuery(value);
+          filterTasks();
+        }}
+      />
       <CommandList>
         {query.length > 1 && <CommandEmpty className="mx-20 my-5">No result found.</CommandEmpty>}
 
         {filteredTasks &&
-          filteredTasks.map((tasks) => (
+          filteredTasks.map((task) => (
             <CommandItem
-              key={tasks.id}
+              onSelect={() => selectTasks(task.id)}
+              key={task.id}
               className="pl-5 py-3 text-gray-600 hover:bg-blue-500 hover:text-white rounded-lg"
             >
-              {tasks.title}
+              {task.title}
             </CommandItem>
           ))}
       </CommandList>
--- blotztask-ui/src/app/dashboard/search/components/search-title.tsx
+++ blotztask-ui/src/app/dashboard/search/components/search-title.tsx
@@ -4,7 +4,7 @@ import { useSearchTaskStore } from '../../store/search-task-store';
 const SearchTitle = () => {
   const { query } = useSearchTaskStore();
 
-  return query.length > 1 ? <p>Searching for "{query}"</p> : <p>Searching for ...</p>;
+  return <>{query.length > 1 ? <p>Searching for "{query}"</p> : <p>Searching for ...</p>}</>;
 };
 
 export default SearchTitle;
