9e126557489ea079fd2f8803ea12509411b69212
w-w01 <155536367+w-w01@users.noreply.github.com>
2025-03-15 12:06:08 +1100

    naming change

--- blotztask-api/Models/UserInfoDTO.cs
+++ blotztask-api/Models/UserInfoDTO.cs
@@ -3,8 +3,8 @@ namespace BlotzTask.Models
     public class UserInfoDTO
     {
         public string Username { get; set; } = string.Empty;
-        public string firstname { get; set; } = string.Empty;
-        public string lastname { get; set; } = string.Empty;
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
         public string Email {get; set;} = string.Empty;
 
         public string Message {get; set;} = string.Empty;
