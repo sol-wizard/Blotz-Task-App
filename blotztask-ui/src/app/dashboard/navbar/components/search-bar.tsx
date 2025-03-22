1bdbe216cdbcef3f5257d39de04bef0311f39339
Nicole <nicolenxn1234@gmail.com>
2025-03-23 09:09:50 +1100

    search page update (#256)

--- blotztask-ui/src/app/dashboard/navbar/components/search-bar.tsx
+++ blotztask-ui/src/app/dashboard/navbar/components/search-bar.tsx
@@ -1,16 +1,22 @@
 import { Command, CommandInput } from '@/components/ui/command';
-import { CommandEmpty, CommandItem, CommandList } from 'cmdk';
 import { useSearchTaskStore } from '../../store/search-task-store';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const SearchBar = () => {
-  const { query, setQuery, filteredTasks, filterTasks, selectTasks } = useSearchTaskStore();
+  const { query, setQuery, filterTasks } = useSearchTaskStore();
   const router = useRouter();
 
   useEffect(() => {
     if (query.length > 0) {
+      if (window.location.pathname !== '/dashboard/search') {
+        localStorage.setItem('previousPage', window.location.pathname);
+      }
       router.push('/dashboard/search');
+    } else {
+      const prev = localStorage.getItem('previousPage');
+      router.push(prev);
+      localStorage.removeItem('previousPage');
     }
   }, [query]);
 
@@ -26,20 +32,6 @@ const SearchBar = () => {
           filterTasks();
         }}
       />
-      <CommandList>
-        {query.length > 1 && <CommandEmpty className="mx-20 my-5">No result found.</CommandEmpty>}
-
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <CommandItem
-              onSelect={() => selectTasks(task.id)}
-              key={task.id}
-              className="pl-5 py-3 text-gray-600 hover:bg-blue-500 hover:text-white rounded-lg"
-            >
-              {task.title}
-            </CommandItem>
-          ))}
-      </CommandList>
     </Command>
   );
 };
