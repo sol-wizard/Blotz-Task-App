0e4d1c7ff065a8d04cb4c3c1cf6b23cd6bcd1d09
Nicole NXN <nicolenxn1234@gmail.com>
2025-03-29 18:20:11 +1100

    fix date picker error

--- blotztask-ui/src/app/dashboard/today/components/task-content.tsx
+++ blotztask-ui/src/app/dashboard/today/components/task-content.tsx
@@ -44,9 +44,10 @@ export default function TaskContent({
       description: data.description ?? '',
       isDone: task.isDone,
       labelId: data.labelId,
-      dueDate: task.dueDate.toLocaleString(),
+      dueDate: data.date.toISOString(),
     };
     onSubmit(editTaskDetails);
+    console.log('updateTask duedate:', editTaskDetails.dueDate);
   };
 
   const [isEditing, setIsEditing] = useState(false);
@@ -151,11 +152,7 @@ export default function TaskContent({
                     >
                       Cancel
                     </button>
-                    <button
-                      type="submit"
-                      className="bg-primary rounded-lg px-3 py-1 text-xs text-white w-20"
-                      onClick={() => console.log('Form errors:', form.formState.errors)}
-                    >
+                    <button type="submit" className="bg-primary rounded-lg px-3 py-1 text-xs text-white w-20">
                       Save
                     </button>
                   </div>
