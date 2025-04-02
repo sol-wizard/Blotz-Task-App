a04a458d53b55701013b155504a67e29c5d54b26
benjaminneoh <benjaminneoh6343@gmail.com>
2025-04-02 14:52:49 +1100

    generic helper function

--- blotztask-ui/src/app/dashboard/store/today-store/util.ts
+++ blotztask-ui/src/app/dashboard/store/today-store/util.ts
@@ -1,16 +1,18 @@
 // A helpfer function to perform an action and then refresh the tasks (so we don't have to repeat the code in multiple places) DRY
-export const performTaskAndRefresh = async (
-  action: () => Promise<unknown>,
+export const performTaskAndRefresh = async <T>(
+  action: () => Promise<T>,
   reloadTasks: () => Promise<unknown>,
   setLoading: (value: boolean) => void
-) => {
+): Promise<T | undefined> => {
   try {
-    setLoading(true); // Set loading to true before performing the action
-    await action();
+    setLoading(true);
+    const result = await action();
     await reloadTasks();
+    return result;
   } catch (error) {
     console.error('Error performing action:', error);
+    return undefined;
   } finally {
-    setLoading(false); // Ensure loading is set to false, even if an error occurs
+    setLoading(false);
   }
 };
