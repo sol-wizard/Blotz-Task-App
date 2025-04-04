fca4c542c94b42a00d8d5fac01c10cb9348f168a
Candicepeiii <141535054+Candicepeiii@users.noreply.github.com>
2025-04-05 10:18:09 +1100

    change the divider length to make it under checkbox (#268)
    Co-authored-by: Candice Chen <z5453581@student.unsw.edu.au>

--- blotztask-ui/src/app/dashboard/today/components/task-content.tsx
+++ blotztask-ui/src/app/dashboard/today/components/task-content.tsx
@@ -2,7 +2,6 @@ import DueDateTag from './due-date-tag';
 import TaskSeparator from '../shared/task-separator';
 import { Pencil } from 'lucide-react';
 import { useState } from 'react';
-import SectionSepreator from './section-separator';
 import { Textarea } from '@/components/ui/textarea';
 import { Input } from 'src/components/ui/task-card-input';
 import { SubmitHandler, useForm } from 'react-hook-form';
@@ -168,7 +167,6 @@ export default function TaskContent({
           </form>
         </Form>
       </div>
-      <SectionSepreator />
     </div>
   );
 }
