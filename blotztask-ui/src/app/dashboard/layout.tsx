d4ab4db3b4e08dc4397be29eba7589a5e23badce
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-04-12 02:37:16 +1000

    Update layout.tsx

--- blotztask-ui/src/app/dashboard/layout.tsx
+++ blotztask-ui/src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <AppSidebar />
         <div className="flex-1 flex flex-col">
           <SidebarTrigger />
-          
+
           <main className="container mx-auto px-6 py-4 flex-1">{children}</main>
         </div>
       </SidebarProvider>
