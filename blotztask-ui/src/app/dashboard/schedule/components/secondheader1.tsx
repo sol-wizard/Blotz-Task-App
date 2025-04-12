d78dd8f3a2e853376be69f3dc7e3c2b209c110bb
Chloexlsx <Chloe850350@gmail.com>
2025-04-12 12:12:55 +1000

    font color and font size

--- blotztask-ui/src/app/dashboard/schedule/components/secondheader1.tsx
+++ blotztask-ui/src/app/dashboard/schedule/components/secondheader1.tsx
@@ -1,6 +1,6 @@
 const SecondHeader1 = ({text}) => {
     return (
-        <p className="text-gray-600 font-extrabold text-xl">{text}</p>
+        <p className="text-gray-700 font-extrabold text-2xl ">{text}</p>
     )
 }
 export default SecondHeader1;
