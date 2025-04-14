cde60241b6448a48aaaa33db96d12b89a817be14
Nicole NXN <nicolenxn1234@gmail.com>
2025-04-14 20:44:20 +1000

    remove use client

--- blotztask-ui/src/model/raw-add-task-dto.ts
+++ blotztask-ui/src/model/raw-add-task-dto.ts
@@ -1,5 +1,3 @@
-'use client';
-
 export interface RawAddTaskDTO {
   title: string;
   description: string;
--- blotztask-ui/src/model/raw-edit-task-dto.ts
+++ blotztask-ui/src/model/raw-edit-task-dto.ts
@@ -1,5 +1,3 @@
-'use client';
-
 export interface RawEditTaskDTO {
   id: number;
   title: string;
