73041ad4214791661a84594633fed989a2eda60c
Ethan <guotaixiao87@gmail.com>
2025-04-17 11:42:45 +1000

    Schedulepage_change the title UI#473 (#278)
    Co-authored-by: Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>

--- blotztask-ui/src/app/dashboard/search/page.tsx
+++ blotztask-ui/src/app/dashboard/search/page.tsx
@@ -2,6 +2,7 @@
 import SearchTitle from './components/search-title';
 import { useSearchTaskStore } from '../store/search-task-store';
 import TaskCard from '../today/components/task-card';
+import { Frown } from 'lucide-react';
 
 export default function Page() {
   const { query, filteredTasks } = useSearchTaskStore();
@@ -37,7 +38,14 @@ export default function Page() {
           </div>
         ))}
 
-      {(filteredTasks.length === 0 || query.length === 0) && <p>No matching task found</p>}
+      {(filteredTasks.length === 0 || query.length === 0) && 
+      
+      <div className="flex flex-col justify-center items-center h-[calc(100vh-80px)]">
+        <Frown color="lightgray" size={58}/>
+        <p className="text-[25] font-bold">No matching task found</p>
+        <p>Try to search another keyword</p>
+      </div>
+        }
     </div>
   );
 }
