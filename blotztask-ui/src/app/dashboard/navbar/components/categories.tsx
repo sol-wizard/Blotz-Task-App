d9d25126998a14787924d0d8642af36808725970
Winn Z <winnwyz@gmail.com>
2025-04-18 15:16:54 +1000

    Make the element unclickable for now

--- blotztask-ui/src/app/dashboard/navbar/components/categories.tsx
+++ blotztask-ui/src/app/dashboard/navbar/components/categories.tsx
@@ -1,38 +1,21 @@
 import { cn } from '@/lib/utils';
 import { LabelDTO } from '@/model/label-dto';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import React from 'react';
 
 export function Categories({ labels }) {
-  const pathname = usePathname();
-
   return (
     <div>
       <ul className="space-y-2">
-        {labels.map((label: LabelDTO) => {
-          const labelUrl = `/dashboard/labels/${label.name.toLowerCase()}`;
-          const isActive = pathname === labelUrl;
-
-          return (
-            <li key={label.name}>
-              <Link
-                href={labelUrl}
-                className={cn(
-                  'flex items-center ml-2 px-4 py-2 w-full rounded-md',
-                  isActive ? 'bg-blue-100  hover:bg-blue-200' : 'hover:bg-blue-200'
-                )}
-              >
-                <span
-                  className="h-4 w-4 rounded-full mr-3"
-                  style={{ backgroundColor: label.color }}
-                  aria-hidden="true"
-                ></span>
-                <span className={isActive ? 'text-primary' : 'text-gray-700'}>{label.name}</span>
-              </Link>
+        {labels.map((label: LabelDTO) => (
+            <li key={label.name} className={cn('flex items-center ml-2 px-4 py-2 w-full rounded-md')}>
+              <span
+                className="h-4 w-4 rounded-full mr-3"
+                style={{ backgroundColor: label.color }}
+                aria-hidden="true"
+              ></span>
+              <span className="text-gray-700">{label.name}</span>
             </li>
-          );
-        })}
+        ))}
       </ul>
     </div>
   );
