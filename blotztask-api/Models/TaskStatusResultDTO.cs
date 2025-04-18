4d398ce34c64ffea1648bbb2a9aecb302abba954
benjaminneoh <benjaminneoh6343@gmail.com>
2025-04-18 16:21:54 +1000

    refactor folder structure into CA pattern

--- blotztask-api/Models/TaskStatusResultDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace BlotzTask.Models
-{
-    public class TaskStatusResultDTO
-    {
-        public int Id { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public string Message {get; set;} = string.Empty;
-    }
-}
