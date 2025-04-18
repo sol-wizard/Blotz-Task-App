5621ae5ce38fd0bb0b0352ebf1daf4e0a18056bf
Winn Z <winnwyz@gmail.com>
2025-04-18 16:51:44 +1000

    Style overdue tasks

--- blotztask-ui/src/app/dashboard/today/components/task-card.tsx
+++ blotztask-ui/src/app/dashboard/today/components/task-card.tsx
@@ -1,11 +1,15 @@
 import { Checkbox } from '@/components/ui/checkbox';
 import React from 'react';
 import TaskContent from './task-content';
+import { isTaskOverdue } from '@/utils/task-utils';
+import { cn } from '@/lib/utils';
 
 const TaskCard = ({ task, handleCheckboxChange, handleTaskEdit, handleTaskDelete, handleTaskDeleteUndo }) => {
+  const isOverdue = isTaskOverdue(task);
+
   return (
     <div>
-      <div className="flex w-full">
+      <div className={cn('flex w-full rounded-lg p-2', isOverdue && 'bg-red-50')}>
         <div className="flex justify-start items-center">
           <Checkbox
             checked={task.isDone}
@@ -18,6 +22,7 @@ const TaskCard = ({ task, handleCheckboxChange, handleTaskEdit, handleTaskDelete
           onSubmit={handleTaskEdit}
           onDelete={(taskId) => handleTaskDelete(taskId)}
           handleTaskDeleteUndo={handleTaskDeleteUndo}
+          isOverdue={isOverdue}
         />
       </div>
     </div>
