fc584b067836f1a2dd570d465ca1877375e338c5
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2025-04-21 15:11:53 +1000

    Clean up (#295)

--- blotztask-ui/src/app/dashboard/search/components/search-title.tsx
+++ blotztask-ui/src/app/dashboard/search/components/search-title.tsx
@@ -2,6 +2,7 @@
 import { useSearchQuery } from '../../../store/search-task-store';
 
 const SearchTitle = () => {
+  //TODO: Move usehook to parent component
   const query = useSearchQuery();
 
   return <>{query.length > 1 ? <p>Searching for &quot;{query}&quot;</p> : <p>Searching for ...</p>}</>;
