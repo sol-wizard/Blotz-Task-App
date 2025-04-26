37cae1823001eba50ae54692862978c711554912
benjaminneoh <benjaminneoh6343@gmail.com>
2025-04-26 12:56:57 +1000

    add swagger and controller

--- blotztask-api-v2/src/BlotzTaskAPI/Program.cs
+++ blotztask-api-v2/src/BlotzTaskAPI/Program.cs
@@ -1,43 +1,24 @@
 using BlotzTask.Application;
 using BlotzTask.Infrastructure;
+using BlotzTaskAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataProtection();
 builder.AddApplicationServices();
 builder.AddInfrastructureServices();
+builder.Services.AddWebServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "BlotzTask API V1");
+    options.RoutePrefix = string.Empty; // ⬅️ This makes Swagger UI load at "/"
+});
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
