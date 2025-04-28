f163953e7ddb2d7fc6402cb4a970edba8a13a58f
Nicole NXN <nicolenxn1234@gmail.com>
2025-04-28 11:54:08 +1000

    use tailwind to replace css

--- blotztask-ui/src/app/auth/components/branding-section.tsx
+++ blotztask-ui/src/app/auth/components/branding-section.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function BrandingSection() {
   return (
-    
-    <div className="flex-[2] relative hidden h-full flex-col items-center justify-center p-10 text-white dark:border-r lg:flex">
-      <img
-        src="/assets/images/logo.png"
-        className="h-auto mb-6" 
-      />
+    <div className="flex-[2] relative hidden h-full flex-col items-center justify-center p-10 lg:flex">
+      <img src="/assets/images/logo.png" className="h-auto mb-6" />
     </div>
   );
 }
--- blotztask-ui/src/app/auth/layout.tsx
+++ blotztask-ui/src/app/auth/layout.tsx
@@ -1,19 +1,10 @@
-import BrandingSection from "./components/branding-section";
-import AuthButton from "./components/auth-button";
+import BrandingSection from './components/branding-section';
+import AuthButton from './components/auth-button';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex container relative h-screen flex-rows items-center justify-center lg:max-w-none lg:px-0">
-
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          backgroundImage: "url('/assets/images/sign_in.png')",
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        }}
-      ></div>
+      <div className="absolute inset-0 z-0 bg-[url('/assets/images/sign_in.png')] bg-cover bg-no-repeat bg-center"></div>
 
       <BrandingSection />
       <AuthButton />
