333ed05db80274037176d708c754f56bc3b80815
WINN <winnwyz@gmail.com>
2025-04-29 17:31:32 +1000

    Remove the spacing on the bottom (#308)

--- blotztask-ui/src/app/auth/layout.tsx
+++ blotztask-ui/src/app/auth/layout.tsx
@@ -3,12 +3,12 @@ import AuthButton from './components/auth-button';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex container relative h-screen flex-rows items-center justify-center lg:max-w-none lg:px-0">
+    <div className="flex container relative h-screen flex-rows items-center justify-center lg:max-w-none p-2">
       <div className="absolute inset-0 z-0 bg-[url('/assets/images/sign_in.png')] bg-cover bg-no-repeat bg-center"></div>
 
       <BrandingSection />
       <AuthButton />
-      <div className="flex-[3] lg:p-8">
+      <div className="flex-[3]">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] relative z-10">
           {children}
         </div>
