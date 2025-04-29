a99a7d9ea4ec4b6aba97429bed2abc2e4b6cb910
Nicole <nicolenxn1234@gmail.com>
2025-04-29 23:04:54 +1000

    Initial clean architecture setup (#302)

--- blotztask-api/Data/Entities/TaskItem.cs
+++ blotztask-api/Data/Entities/TaskItem.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlotzTask.Data.Entities
 {
