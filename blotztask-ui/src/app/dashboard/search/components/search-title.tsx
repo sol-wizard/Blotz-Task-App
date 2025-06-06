80c3e745aaf34e36144ec646bb028985d42af67a
Candice Chen <z5453581@student.unsw.edu.au>
2025-06-06 22:15:23 +1000

    Adding section-separator between tasks

--- blotztask-ui/src/app/dashboard/search/components/search-title.tsx
+++ blotztask-ui/src/app/dashboard/search/components/search-title.tsx
@@ -5,7 +5,17 @@ const SearchTitle = () => {
   //TODO: Move usehook to parent component
   const query = useSearchQuery();
 
-  return <>{query.length > 1 ? <p>Searching for &quot;{query}&quot;</p> : <p>Searching for ...</p>}</>;
+  return (
+    <>
+      {query.length > 1 ? (
+        <p style={{ fontWeight: '2rem', fontSize: '2rem' }}>
+          Searching for &quot;{query}&quot;
+        </p>
+      ) : (
+        <p style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>Searching for ...</p>
+      )}
+    </>
+  );
 };
 
 export default SearchTitle;
