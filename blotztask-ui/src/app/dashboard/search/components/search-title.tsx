2497ee0411133237ee67931c79af2a890b3b12eb
WINN <winnwyz@gmail.com>
2025-06-10 23:06:48 +1000

    Refine select title UI (#336)
    Co-authored-by: 推推车 <ppcar@tuituichedeMacBook-Pro.local>

--- blotztask-ui/src/app/dashboard/search/components/search-title.tsx
+++ blotztask-ui/src/app/dashboard/search/components/search-title.tsx
@@ -5,7 +5,22 @@ const SearchTitle = () => {
   //TODO: Move usehook to parent component
   const query = useSearchQuery();
 
-  return <>{query.length > 1 ? <p>Searching for &quot;{query}&quot;</p> : <p>Searching for ...</p>}</>;
+  return (
+    <>
+      {query.length > 0 ? (
+        <h2 className="mt-3 text-2xl font-semibold text-gray-600 mb-8 pl-5">
+          Searching for “{query}”
+        </h2>
+      ) : (
+        <h2 className="text-2xl font-semibold text-gray-400 mb-4">
+          Searching for ...
+        </h2>
+      )}
+    </>
+  );
 };
 
 export default SearchTitle;
+
+// test
+
