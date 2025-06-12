a75f5401c745d61b126d8137933bc2d4b5c36672
Nicole <nicolenxn1234@gmail.com>
2025-06-12 20:16:38 +1000

    deploy staging next js app (#352)

--- blotztask-ui/next.config.mjs
+++ blotztask-ui/next.config.mjs
@@ -3,6 +3,8 @@
 //Disable react strict mode to avoid double rendering in the signalR conenction (this is a temporary fix for the signalR connection, we should find solution for this)
 const nextConfig = {
     reactStrictMode: false,
+    distDir: 'build',
+    output: 'standalone',
 };
 
 export default nextConfig;
