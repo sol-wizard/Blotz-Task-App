9a15780fadd57350592672f14a2242f1ab8b6dbf
Alicetang1 <103195239+Alicetang1@users.noreply.github.com>
2025-06-14 16:42:09 +0930

    update task form validation (#334)
    Co-authored-by: Aug-8 <130132558+sol-wizard@users.noreply.github.com>

--- blotztask-ui/src/app/dashboard/shared/forms/task-form-schema.ts
+++ blotztask-ui/src/app/dashboard/shared/forms/task-form-schema.ts
@@ -12,9 +12,19 @@ const timeSchema = z.string().refine(
 );
 
 export const taskFormSchema = z.object({
-  title: z.string().min(1, 'Title is required').max(50, 'Title cannot exceed 50 characters.'),
-  description: z.string().max(200, 'Description cannot exceed 200 characters.'),
-  date: z.date(),
-  labelId: z.number(),
-  time: timeSchema.optional(),
+  title: z.string().min(1,'Title is required').max(50, 'Title cannot exceed 50 characters.'),
+  description: z.string().max(200, 'Description cannot exceed 200 characters.').optional(),
+  
+  date: z.date({
+    required_error: 'Date is required',
+    invalid_type_error: 'Date must be a valid date',
+  }),
+
+  labelId: z
+    .number({
+      required_error: 'Label is required',
+      invalid_type_error: 'Label must be a number',
+    }),
+
+  time: timeSchema.optional(), 
 });
