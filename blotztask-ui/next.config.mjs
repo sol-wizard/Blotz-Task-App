5d1a9e47d8d8f6f538522686af1aef56fe0d1f12
Nicole <nicolenxn1234@gmail.com>
2025-06-15 15:39:24 +1000

    remove disdir build (#356)

--- blotztask-ui/next.config.mjs
+++ blotztask-ui/next.config.mjs
@@ -3,7 +3,6 @@
 //Disable react strict mode to avoid double rendering in the signalR conenction (this is a temporary fix for the signalR connection, we should find solution for this)
 const nextConfig = {
     reactStrictMode: false,
-    distDir: 'build',
     output: 'standalone',
 };
 
