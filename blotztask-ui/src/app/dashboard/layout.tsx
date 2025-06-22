3acd2261eed8a7412a9c67ead79eeebd4dbf0ec7
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2025-06-22 21:37:50 +1000

    Remove warning (#366)

--- blotztask-ui/src/app/dashboard/layout.tsx
+++ blotztask-ui/src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { AppSidebar } from './navbar/side-nav';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { useSession } from 'next-auth/react';
+import { EnvironmentIndicator } from './components/environment-indicator';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const { status } = useSession();
@@ -20,9 +21,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <AppSidebar />
         <div className="flex-1 flex flex-col">
           <SidebarTrigger />
-          <div className="bg-yellow-50 text-yellow-800 border border-yellow-200 text-lg rounded-md px-3 py-4 mt-1 mx-16 font-bold">
-            🚧 This project is a work in progress. A more stable production environment will be rolled out soon — expect some bugs or changes in the meantime.
-          </div>
+          <EnvironmentIndicator />
           <main className="container mx-auto px-6 py-4 flex-1">{children}</main>
         </div>
       </SidebarProvider>
