4c2f6237672d235545daf83ac261178c80577485
Nicole <nicolenxn1234@gmail.com>
2025-07-15 11:52:05 +1000

    Setup auth protection for mobile app (#385)

--- blotztask-mobile/app/_layout.tsx
+++ blotztask-mobile/app/_layout.tsx
@@ -3,12 +3,19 @@ import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css"; // If using Expo Web / Tailwind
 import { theme } from "../constants/theme";
+import React from "react";
+import { AuthProvider } from "../contexts/AuthProvider";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <PaperProvider theme={theme}>
-        <Stack screenOptions={{ headerShown: false }} />  
+        <AuthProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="login" options={{ headerShown: false }} />
+          </Stack>
+        </AuthProvider>
       </PaperProvider>
     </SafeAreaProvider>
   );
