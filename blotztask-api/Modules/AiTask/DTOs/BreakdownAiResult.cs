416a190320353991d1ca1af108af9f61e8d8995b
w-w01 <155536367+w-w01@users.noreply.github.com>
2025-07-19 01:31:37 +0800

    controller and bug fix

--- blotztask-api/Modules/AiTask/DTOs/BreakdownAiResult.cs
+++ blotztask-api/Modules/AiTask/DTOs/BreakdownAiResult.cs
@@ -1,20 +1,24 @@
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace BlotzTask.Modules.AiTask.DTOs;
 
 public class BreakdownAiResult
 {
+    [JsonPropertyName("action")]
     public string Action { get; set; } = default!;
+
+    [JsonPropertyName("subtasks")]
     public List<BreakdownAiSubtask>? Subtasks { get; set; }
 }
 
 public class BreakdownAiSubtask
 {
+    [JsonPropertyName("title")]
     public string Title { get; set; } = default!;
+
+    [JsonPropertyName("description")]
     public string Description { get; set; } = default!;
+
+    [JsonPropertyName("label")]
     public string Label { get; set; } = default!;
 }
--- blotztask-api/Modules/AiTask/DTOs/BreakdownDTO.cs
+++ blotztask-api/Modules/AiTask/DTOs/BreakdownDTO.cs
@@ -4,6 +4,11 @@ namespace BlotzTask.Modules.AiTask.DTOs;
 public class BreakdownResponseDto
 {
     public bool IsSplit { get; set; }
-    public TaskItemDto? OriginalTask { get; set; }
     public List<TaskItemDto>? Subtasks { get; set; }
+}
+
+public class BreakdownRequestDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
 }
