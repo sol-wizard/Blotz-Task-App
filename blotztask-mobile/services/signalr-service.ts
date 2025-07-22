ba468a8174e2d3b2f40ac8a1f83b9ba47c95fc5b
WINN <winnwyz@gmail.com>
2025-07-22 22:39:42 +1000

    Signal r mobile (#393)

---
+++ blotztask-mobile/services/signalr-service.ts
@@ -0,0 +1,30 @@
+import * as signalR from "@microsoft/signalr";
+
+const config = {
+  API_BASE_URL: process.env.EXPO_PUBLIC_URL,
+};
+
+const SIGNALR_HUBS_CHAT = `${config.API_BASE_URL}/chatHub`;
+
+export const signalRService = {
+  createConnection: () => {
+    console.log(
+      "Attempting to create SignalR connection to:",
+      SIGNALR_HUBS_CHAT
+    );
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl(SIGNALR_HUBS_CHAT)
+      .withAutomaticReconnect()
+      .build();
+    return connection;
+  },
+
+  invoke: async <T = unknown>(
+    connection: signalR.HubConnection,
+    method: string,
+    ...args: unknown[]
+  ): Promise<T> => {
+    if (!connection) throw new Error("Connection not initialized.");
+    return connection.invoke<T>(method, ...args);
+  },
+};
