38fb346aaf88c67ab30ae0a35747223b3beef81b
nicole-nxn <nicolenxn1234@gmail.com>
2025-08-06 14:55:04 +1000

    fix error

--- blotztask-mobile/src/feature/task/calendars/calendar-screen.tsx
+++ blotztask-mobile/src/feature/task/calendars/calendar-screen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, FlatList, Task } from "react-native";
+import { SafeAreaView, FlatList } from "react-native";
 import {
   CalendarProvider,
   WeekCalendar,
