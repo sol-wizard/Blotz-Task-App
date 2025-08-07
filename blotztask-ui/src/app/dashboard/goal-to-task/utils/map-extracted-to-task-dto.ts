653353b5230afb3875cf71e9cfb0d5a702bf6f19
Winn Z <winnwyz@gmail.com>
2025-08-07 11:30:26 +1000

    Fix frontend mapping

--- blotztask-ui/src/app/dashboard/goal-to-task/utils/map-extracted-to-task-dto.ts
+++ blotztask-ui/src/app/dashboard/goal-to-task/utils/map-extracted-to-task-dto.ts
@@ -2,6 +2,7 @@ import { ExtractedTask } from '@/model/extracted-task-dto';
 import { TaskDetailDTO } from '@/model/task-detail-dto';
 
 export function mapExtractedToTaskDetail(extractedTask: ExtractedTask): TaskDetailDTO {
+  console.log("in mapExtractedToTaskDetail",extractedTask)
   return {
     //TODO: This is just a temporary id, we not using id for now, just to fulfill the DTO, we will need new dto for it 
       id: Date.now() + Math.floor(Math.random() * 10),
@@ -9,7 +10,7 @@ export function mapExtractedToTaskDetail(extractedTask: ExtractedTask): TaskDeta
       title: extractedTask.title,
       isDone: false,
       label: extractedTask.label,
-      endTime: new Date(extractedTask.due_date),
+      endTime: new Date(extractedTask.endTime),
       hasTime: false,
   };
 }
