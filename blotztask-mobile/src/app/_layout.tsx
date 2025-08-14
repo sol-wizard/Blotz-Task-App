a8eb6e011a6229d546058c5199aa76a3197fd413
Nicole <nicolenxn1234@gmail.com>
2025-08-15 00:15:09 +1000

    Setup create task form (#418)

--- blotztask-mobile/src/app/_layout.tsx
+++ blotztask-mobile/src/app/_layout.tsx
@@ -1,36 +1,24 @@
-import 'react-native-gesture-handler';
-import 'react-native-reanimated';
+import "react-native-gesture-handler";
+import "react-native-reanimated";
 
 import { Stack } from "expo-router";
 import { MD3LightTheme, PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../../global.css";
 import React from "react";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <PaperProvider theme={theme}>
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen 
-                name="index" 
-                options={{ headerShown: false }} 
-              />
-              <Stack.Screen 
-                name="onboarding" 
-                options={{ headerShown: false }} 
-              />
-              <Stack.Screen 
-                name="(auth)" 
-                options={{ headerShown: false }} 
-              />
-              <Stack.Screen 
-                name="(protected)" 
-                options={{ headerShown: false }} 
-              />
-            </Stack>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+          </Stack>
         </PaperProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
---
+++ blotztask-mobile/src/feature/task/task-creation/create-task-bottom-sheet.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useCallback } from "react";
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import TaskCreationForm from "../task-creation/task-creation-form";
+import { Portal } from "react-native-paper";
+import { View } from "react-native";
+
+export const CreateTaskBottomSheet = ({
+  isVisible,
+  onClose,
+}: {
+  isVisible: boolean;
+  onClose: (isVisible: boolean) => void;
+}) => {
+  const taskCreationBottomSheetRef = useRef<BottomSheet>(null);
+
+  // const handleTaskCreationSheetClose = () => {
+  //   taskCreationBottomSheetRef.current?.close();
+  // };
+
+  // const handleTaskCreationSheetOpen = () => {
+  //   taskCreationBottomSheetRef.current?.snapToIndex(0);
+  // };
+
+  const handleSheetChange = useCallback(
+    (index: number) => {
+      if (index === -1) {
+        onClose(false);
+      }
+    },
+    [onClose]
+  );
+
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        {...props}
+        disappearsOnIndex={-1}
+        appearsOnIndex={0}
+        pressBehavior="close"
+      />
+    ),
+    []
+  );
+
+  return (
+    <Portal>
+      <View className="absolute inset-0 z-50">
+        <BottomSheet
+          ref={taskCreationBottomSheetRef}
+          index={isVisible ? 0 : -1}
+          snapPoints={["55%"]}
+          keyboardBlurBehavior="restore"
+          backdropComponent={renderBackdrop}
+          onChange={handleSheetChange}
+          enablePanDownToClose
+          onClose={() => onClose(false)}
+        >
+          <BottomSheetView style={{ padding: 16 }}>
+            <TaskCreationForm
+              handleTaskCreationSheetClose={handleSheetChange}
+              // handleTaskCreationSheetOpen={handleTaskCreationSheetOpen}
+            />
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </Portal>
+  );
+};
