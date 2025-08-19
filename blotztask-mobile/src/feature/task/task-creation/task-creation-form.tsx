f4cb6e518cb5833f5597293edd7c7716b68f0bee
Nicole <nicolenxn1234@gmail.com>
2025-08-19 20:55:56 +1000

    Create ai breakdown page (#432)

--- blotztask-mobile/src/feature/task/task-creation/task-creation-form.tsx
+++ blotztask-mobile/src/feature/task/task-creation/task-creation-form.tsx
@@ -4,7 +4,7 @@ import { RepeatMenu } from "@/feature/task/task-creation/repeat-menu";
 import { FormTextInput } from "@/shared/components/ui/form-text-input";
 import AddTaskFormField, {
   taskCreationSchema,
-} from "@/feature/task/services/task-form-schema";
+} from "@/feature/task/services/task-creation-form-schema";
 import { addTaskItem } from "@/feature/task/services/task-service";
 import { toAddTaskItemDTO } from "@/feature/task/services/util/util";
 import { zodResolver } from "@hookform/resolvers/zod";
