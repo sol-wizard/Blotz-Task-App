fa294279c4b51786deb8633f9bd55e131b01484d
nicole-nxn <nicolenxn1234@gmail.com>
2025-08-19 17:42:14 +1000

    add form

--- blotztask-mobile/src/feature/breakdown/components/breakdown-task-card.tsx
+++ blotztask-mobile/src/feature/breakdown/components/breakdown-task-card.tsx
@@ -4,6 +4,12 @@ import { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { COLORS } from "@/shared/constants/colors";
 import { SubTask } from "../models/subtask";
+import { zodResolver } from "@hookform/resolvers/zod";
+import EditTaskFormField, {
+  taskEditFormSchema,
+} from "../services/breakdown-task-edit-form-schema";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import { FormTextInput } from "@/shared/components/ui/form-text-input";
 
 export const BreakdownTaskCard = ({
   parentTaskId,
@@ -19,36 +25,49 @@ export const BreakdownTaskCard = ({
     console.log("sub task added!", task, "parentTaskId:", parentTaskId);
   };
 
-  const handleEditTask = () => {
-    console.log("sub task edited");
-  };
+  const form = useForm<EditTaskFormField>({
+    resolver: zodResolver(taskEditFormSchema),
+    mode: "onBlur",
+    defaultValues: {
+      title: subTask.title,
+    },
+  });
 
   return (
-    <View className="flex-row w-full items-center justify-between">
-      <View className="flex-row items-center rounded-2xl bg-white mb-3 px-4 py-3 flex-1 border border-gray-300">
-        <Checkbox
-          status={isChecked ? "checked" : "unchecked"}
-          onPress={() => handleAddTask(subTask)}
-        />
-        <View className="w-[5px] bg-gray-300 h-full min-h-[40px] mr-4 rounded-md" />
-        <View className="flex-col">
-          <TextInput
-            value={subTask?.title}
-            onChangeText={handleEditTask}
-            style={{ fontSize: 16, fontWeight: "600" }}
-            multiline={true}
-            scrollEnabled={false}
+    <FormProvider {...form}>
+      <View className="flex-row w-full items-center justify-between">
+        <View className="flex-row items-center rounded-2xl bg-white mb-3 px-4 py-3 flex-1 border border-gray-300">
+          <Checkbox
+            status={isChecked ? "checked" : "unchecked"}
+            onPress={() => handleAddTask(subTask)}
           />
-          <View className="flex-row my-1">
-            <MaterialIcons name="schedule" size={20} color={COLORS.primary} />
-            {subTask.duration && (
-              <Text className="text-base text-primary ml-2">
-                {subTask.duration}
-              </Text>
-            )}
+          <View className="w-[5px] bg-gray-300 h-full min-h-[40px] mr-4 rounded-md" />
+          <View className="flex-col">
+            <Controller
+              control={form.control}
+              name="title"
+              render={({ field: { value, onChange } }) => (
+                <TextInput
+                  value={value}
+                  onChangeText={onChange}
+                  style={{ fontSize: 16, fontWeight: "600" }}
+                  multiline={true}
+                  scrollEnabled={false}
+                />
+              )}
+            />
+
+            <View className="flex-row my-1">
+              <MaterialIcons name="schedule" size={20} color={COLORS.primary} />
+              {subTask.duration && (
+                <Text className="text-base text-primary ml-2">
+                  {subTask.duration}
+                </Text>
+              )}
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </FormProvider>
   );
 };
