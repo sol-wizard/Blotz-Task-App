efd39ade62d247c354b6055c30ccdf2fa954e053
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-08-21 22:36:14 +1000

    Make dates nullable (#435)

--- blotztask-api/Modules/Tasks/Domain/Entities/DeletedTaskItem.cs
+++ blotztask-api/Modules/Tasks/Domain/Entities/DeletedTaskItem.cs
@@ -10,7 +10,7 @@ public class DeletedTaskItem
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public DateTimeOffset EndTime { get; set; }
+    public DateTimeOffset? EndTime { get; set; }
     public bool IsDone { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
