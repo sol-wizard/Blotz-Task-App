69ea5d3a327cacf0bf80f0a834892c70c4a2cde4
JoeLi <liyuanzhuo0704@gmail.com>
2025-08-21 16:18:12 +1000

    more rollback

--- blotztask-api/Modules/Tasks/TaskController.cs
+++ blotztask-api/Modules/Tasks/TaskController.cs
@@ -24,7 +24,7 @@ public class TaskController : ControllerBase
     public async Task<ActionResult<List<TaskItemDto>>> GetAllTask(CancellationToken cancellationToken)
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
+        
         if (string.IsNullOrEmpty(userId))
         {
             throw new UnauthorizedAccessException("Could not find user id from token");
@@ -53,7 +53,7 @@ public class TaskController : ControllerBase
     {
         return Ok(await _taskService.GetTaskById(id));
     }
-
+        
     //TODO: change the route "due-date" to "by-date"
     [HttpGet("due-date")]
     public async Task<IActionResult> GetTaskByDate([FromQuery] DateTime startDateUtc)
@@ -64,7 +64,7 @@ public class TaskController : ControllerBase
         {
             throw new UnauthorizedAccessException("Could not find user id from Http Context");
         }
-
+            
         DateTime endDateUtc = startDateUtc.AddDays(1);
         return Ok(await _taskService.GetTaskByDate(startDateUtc, endDateUtc, userId));
     }
@@ -127,7 +127,7 @@ public class TaskController : ControllerBase
     }
 
     [HttpPost("{id}/undo-delete")]
-    public async Task<IActionResult> RestoreFromTrash(int id)
+    public async Task<IActionResult> RestoreFromTrash(int id) 
     {
         var result = await _taskService.RestoreFromTrashAsync(id);
         if (!result.Success)
@@ -153,7 +153,7 @@ public class TaskController : ControllerBase
         {
             throw new UnauthorizedAccessException("Could not find user id from Http Context");
         }
-
+            
         return Ok(await _taskService.GetScheduledTasks(timeZone, todayDate, userId));
     }
 
