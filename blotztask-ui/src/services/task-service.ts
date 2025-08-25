ba415c774a686f29812026f0b30196059aa96fb2
nicole-nxn <nicolenxn1234@gmail.com>
2025-08-25 22:37:46 +1000

    fix by-date error

--- blotztask-ui/src/services/task-service.ts
+++ blotztask-ui/src/services/task-service.ts
@@ -23,7 +23,7 @@ export const fetchTaskItemsDueToday = async (): Promise<TaskDetailDTO[]> => {
   const startDateUTC = new Date(new Date().setHours(0, 0, 0, 0)).toISOString();
 
   const result = await fetchWithAuth<TaskDetailDTO[]>(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL_WITH_API}/Task/due-date?startDateUTC=${encodeURIComponent(startDateUTC)}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL_WITH_API}/Task/by-date?startDateUTC=${encodeURIComponent(startDateUTC)}`,
     {
       method: 'GET',
       headers: {
