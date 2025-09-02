c0da5003caa8fa52a5a1d222000fe3e1977a041d
Winn Z <winnwyz@gmail.com>
2025-09-02 11:04:16 +1000

    Correct the namespaces under chatgoalplanner and chattaskgenerator

--- blotztask-api/Modules/ChatGoalPlanner/DTOs/ConversationMessage.cs
+++ blotztask-api/Modules/ChatGoalPlanner/DTOs/ConversationMessage.cs
@@ -1,4 +1,4 @@
-namespace BlotzTask.Modules.GoalPlannerChat.Dtos;
+namespace BlotzTask.Modules.ChatGoalPlanner.DTOs;
 
 // This class is strongly typed to client end (frontend) expected messaging object
 // Any changes of structure in this class need to also change in the client end (ConversationMessage)
