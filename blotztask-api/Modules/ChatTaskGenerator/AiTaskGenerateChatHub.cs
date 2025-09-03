96d24250e43b381b64c65e6994b7a1f592ac0dcd
Winn Z <winnwyz@gmail.com>
2025-09-03 11:58:09 +1000

    Remove conversation id

--- blotztask-api/Modules/ChatTaskGenerator/AiTaskGenerateChatHub.cs
+++ blotztask-api/Modules/ChatTaskGenerator/AiTaskGenerateChatHub.cs
@@ -41,7 +41,6 @@ public class AiTaskGenerateChatHub : Hub
         {
             Sender = user,
             Content = message,
-            ConversationId = conversationId,
             Timestamp = DateTime.UtcNow,
             IsBot = false,
         };
