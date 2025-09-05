7787dfd3b920187c126a7054b18301d5187e4e79
Rachel <43347454+RachelYrt@users.noreply.github.com>
2025-09-05 23:49:05 +1000

    Edit task UI (#461)
    Co-authored-by: nicole-nxn <nicolenxn1234@gmail.com>
    Co-authored-by: Ben Neoh [SSW] <benjaminneoh6343@gmail.com>

--- blotztask-mobile/src/shared/components/ui/form-text-input.tsx
+++ blotztask-mobile/src/shared/components/ui/form-text-input.tsx
@@ -1,16 +1,19 @@
-import { BottomSheetTextInput } from "@gorhom/bottom-sheet";
-import { Controller } from "react-hook-form";
+import { BottomSheetTextInput } from '@gorhom/bottom-sheet'
+import { Controller } from 'react-hook-form'
+import { TextInputProps } from 'react-native'
 
 export function FormTextInput({
   name,
   placeholder,
   control,
-  className = "",
+  className = '',
+  inputProps,
 }: {
-  name: string;
-  placeholder?: string;
-  control: any;
-  className?: string;
+  name: string
+  placeholder: string
+  control: any
+  className?: string
+  inputProps?: TextInputProps
 }) {
   return (
     <Controller
@@ -22,8 +25,9 @@ export function FormTextInput({
           value={value}
           onChangeText={onChange}
           className={`bg-[#F9FAFB] rounded-2xl h-12 px-2 ${className}`}
+          {...inputProps}
         />
       )}
     />
-  );
+  )
 }
