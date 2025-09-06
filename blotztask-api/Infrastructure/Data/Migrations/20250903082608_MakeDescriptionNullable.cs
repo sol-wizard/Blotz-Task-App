3df2d18e0f6aec78f16cedd7d78bd89a7a2fc0ea
George Lee <141626855+Geoseanlee@users.noreply.github.com>
2025-09-06 14:16:59 +1000

    Restructure TaskItem Nullable Fields and Required Title (#460)
    Co-authored-by: Geoseanlee <>
    Co-authored-by: JoeLi <137287434+Phenomenax@users.noreply.github.com>
    Co-authored-by: WINN <winnwyz@gmail.com>
    Co-authored-by: JoeLi <liyuanzhuo0704@gmail.com>

---
+++ blotztask-api/Infrastructure/Data/Migrations/20250903082608_MakeDescriptionNullable.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlotzTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDescriptionNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_Labels_LabelId",
+                table: "TaskItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LabelId",
+                table: "TaskItems",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasTime",
+                table: "TaskItems",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_Labels_LabelId",
+                table: "TaskItems",
+                column: "LabelId",
+                principalTable: "Labels",
+                principalColumn: "LabelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskItems_Labels_LabelId",
+                table: "TaskItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LabelId",
+                table: "TaskItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasTime",
+                table: "TaskItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskItems_Labels_LabelId",
+                table: "TaskItems",
+                column: "LabelId",
+                principalTable: "Labels",
+                principalColumn: "LabelId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
