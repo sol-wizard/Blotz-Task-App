8be5dddb336715dfe20b61ff68c3c82bfcf80fde
Nicole <nicolenxn1234@gmail.com>
2025-09-08 00:01:46 +1000

    set up voice input (#477)

--- blotztask-mobile/src/shared/components/ui/typing-area.tsx
+++ blotztask-mobile/src/shared/components/ui/typing-area.tsx
@@ -1,4 +1,5 @@
-import { TextInput, View } from "react-native";
+import { useVoiceInput } from "@/shared/util/useVoiceInput";
+import { TextInput, View, Pressable } from "react-native";
 import { IconButton } from "react-native-paper";
 
 export const TypingArea = ({
@@ -10,11 +11,22 @@ export const TypingArea = ({
   setText: (text: string) => void;
   handleSend: () => void;
 }) => {
+  const { startListening, stopAndGetText, isListening } = useVoiceInput();
+
+  const handleMicPressOut = async () => {
+    const spoken = await stopAndGetText();
+    if (spoken) {
+      const newText = text?.length ? `${text.trim()} ${spoken}` : spoken;
+      setText(newText);
+    }
+  };
+
   return (
     <View
-      className="flex-row items-center align-top px-3 border-t border-l border-r border-gray-200 bg-white rounded-t-2xl"
+      className="flex-row items-end align-top px-3 border-t border-l border-r border-gray-200 bg-white rounded-t-2xl pt-2"
       style={{
-        height: 60,
+        minHeight: 60,
+        maxHeight: 150,
         shadowColor: "#000",
         shadowOffset: { width: 0, height: -5 },
         shadowOpacity: 0.1,
@@ -25,14 +37,19 @@ export const TypingArea = ({
       <TextInput
         className="flex-1 text-base text-gray-900 h-full"
         placeholder="Remind me 10 mins before my interview..."
+        multiline
         value={text}
         onChangeText={setText}
         onSubmitEditing={handleSend}
         placeholderTextColor="#aaa"
         returnKeyType="send"
       />
-      <IconButton icon="send" size={20} onPress={handleSend} />
-      <IconButton icon="microphone" size={20} onPress={() => {}} />
+      <View className="flex-row justify-end">
+        <IconButton icon="send" size={20} onPress={handleSend} />
+        <Pressable onLongPress={startListening} onPressOut={handleMicPressOut} delayLongPress={250}>
+          <IconButton icon={isListening ? "microphone-outline" : "microphone"} size={20} />
+        </Pressable>
+      </View>
     </View>
   );
 };
