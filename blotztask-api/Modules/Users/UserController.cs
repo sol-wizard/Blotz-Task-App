5d3e19cc0b0bf100c84b53e1fc01fc37f6e4b7cf
Ben Neoh [SSW] <benjaminneoh6343@gmail.com>
2025-09-09 00:04:35 +1000

    implement auth0 sync function

--- blotztask-api/Modules/Users/UserController.cs
+++ blotztask-api/Modules/Users/UserController.cs
@@ -1,8 +1,4 @@
-using System.Security.Claims;
 using BlotzTask.Modules.Users.Commands;
-using BlotzTask.Modules.Users.DTOs;
-using BlotzTask.Modules.Users.Services;
-using BlotzTask.Shared.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,50 +7,8 @@ namespace BlotzTask.Modules.Users;
 [ApiController]
 [Route("api/[controller]")]
 [Authorize]
-public class UserController(IUserService userService, AddUserCommandHandler addUserCommandHandler, ILogger<UserController> logger) : ControllerBase
+public class UserController(AddUserCommandHandler addUserCommandHandler, ILogger<UserController> logger) : ControllerBase
 {
-    [HttpGet("current-user-info")]
-    [Obsolete("This controller is not in use on mobile app and will be removed/refactor later.")]
-    public async Task<IActionResult> GetCurrentUserInfo()
-    {
-        // Use UserInfoService to get User info.
-        try{
-            // Extract user ID from claims
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (string.IsNullOrEmpty(userId))
-            {
-                return Unauthorized(new { message = "User not authenticated or missing required claims" });
-            }
-
-            var userInfo = await userService.GetCurrentUserInfoAsync(userId);
-            var message = userInfo.Message;
-            // return result
-            return Ok(new ResponseWrapper<UserInfoDto>(userInfo, message, true));
-        }catch(Exception){
-            throw;
-        }
-        
-    }
-    
-    [HttpPost("register")]
-    [AllowAnonymous]
-    public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-        
-        var result = await userService.RegisterUserAsync(request);
-
-        if (!result.Succeeded)
-        {
-            return BadRequest(result.Errors);
-        }
-
-        return Ok(new { message = "User registered successfully." });
-    }
-    
     [HttpPost("user-sync")]
     [AllowAnonymous]
     [ApiExplorerSettings(IgnoreApi = true)]
