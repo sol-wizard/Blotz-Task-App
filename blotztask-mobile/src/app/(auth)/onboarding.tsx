bfa7a414e054773f7129baa3e70d4e87642c9591
Ben Neoh [SSW] <benjaminneoh6343@gmail.com>
2025-09-10 01:17:19 +1000

    refactor onboarding page

--- blotztask-mobile/src/app/(auth)/onboarding.tsx
+++ blotztask-mobile/src/app/(auth)/onboarding.tsx
@@ -1,40 +1,8 @@
 import React from "react";
-import { View, StatusBar } from "react-native";
-import { Button } from "react-native-paper";
-import { useRouter } from "expo-router";
-import LottieView from "lottie-react-native";
-import { useAuth0 } from "react-native-auth0";
-import * as SecureStore from "expo-secure-store";
-import { AUTH_TOKEN_KEY } from "@/shared/constants/token-key";
+import { View, StatusBar, Image } from "react-native";
+import GetStartedButton from "@/feature/auth/components/get-started-button";
 
 export default function OnboardingScreen() {
-  const router = useRouter();
-
-  const LoginButton = () => {
-    const {authorize} = useAuth0();
-    const onPress = async () => {  
-      try {
-          const result = await authorize({
-            audience: process.env.EXPO_PUBLIC_AUTH0_AUDIENCE,
-          });          
-          // Check if we have a valid result with access token
-          if (result && result.accessToken) {
-            // Save the access token to secure storage
-            await SecureStore.setItemAsync(AUTH_TOKEN_KEY, result.accessToken);
-            console.log("Access token saved to storage");
-            
-            // Redirect to protected page
-            router.replace("/(protected)");
-          } else {
-            console.error("No access token received from Auth0");
-          }
-        } catch (e) {
-            console.error("Auth0 authorization error:", e);
-        }
-    };
-    
-    return <Button onPress={onPress} mode="contained">Log In</Button>
-  }
 
   return (
     <>
@@ -48,26 +16,24 @@ export default function OnboardingScreen() {
           paddingHorizontal: 40,
         }}
       >
-        {/* Onboarding Animation */}
+
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-          <LottieView
-            source={require("../../../assets/images/onboarding.json")}
-            autoPlay
-            loop
+          <Image
+            source={require("../../../assets/images/blotz-logo.png")}
             style={{ width: 240, height: 240 }}
+            resizeMode="contain"
           />
         </View>
 
-        {/* Get Started Button */}
         <View
           style={{
             width: "100%",
             paddingBottom: 60,
           }}
         >
-          <LoginButton />
+          <GetStartedButton />
         </View>
       </View>
     </>
