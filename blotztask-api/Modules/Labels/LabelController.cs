923968424c2cfe8c6e8d5d22e7e71c458f051170
Ben Neoh [SSW] <87377903+Ben0189@users.noreply.github.com>
2025-09-14 23:03:17 +1000

    Fix backend route (#492)
    Co-authored-by: Aug-8 <130132558+sol-wizard@users.noreply.github.com>

--- blotztask-api/Modules/Labels/LabelController.cs
+++ blotztask-api/Modules/Labels/LabelController.cs
@@ -1,54 +1,19 @@
-﻿using BlotzTask.Modules.Labels.Commands;
-using BlotzTask.Modules.Labels.Queries;
-using BlotzTask.Modules.Labels.Services;
+﻿using BlotzTask.Modules.Labels.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using AddLabelDto = BlotzTask.Modules.Labels.DTOs.AddLabelDto;
 
 namespace BlotzTask.Modules.Labels;
 
 [Authorize]
 [ApiController]
 [Route("api/[controller]")]
-public class LabelController(ILabelService _labelService, AddLabelCommandHandler addlabelCommandHandler, GetAllLabelsQueryHandler getAlllabelsQueryHandler) : ControllerBase
+public class LabelController(GetAllLabelsQueryHandler getAllLabelsQueryHandler) : ControllerBase
 {
 
     [HttpGet]
     public async Task<IActionResult> GetAllLabels(CancellationToken ct)
     {
-        var result = await getAlllabelsQueryHandler.Handle(ct);
+        var result = await getAllLabelsQueryHandler.Handle(ct);
         return Ok(result);
-    }
-    
-        
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetLabelById(int id)
-    {
-        return Ok(await _labelService.GetLabelById(id)); 
-    }
-
-
-
-    [HttpPost]
-    [Obsolete("This endpoint is not in used")]
-    public async Task<IActionResult> AddLabel([FromBody] AddLabelDto addLabels, CancellationToken ct)
-    {
-
-        var name = addLabels.Name;
-        var command = new AddLabelCommand
-        {
-            Name = name,
-            Color = addLabels.Color,
-            Description = addLabels.Description,
-        };
-        var result = await addlabelCommandHandler.Handle(command, ct);
-        if (result == null)
-        {
-            throw new InvalidOperationException($"Failed to add label {name}.");
-
-        }
-
-        return Ok(result);
-        
     }
 }
