3378c2f15679b2eeca42d87dfefd8346e184a699
nicole-nxn <nicolenxn1234@gmail.com>
2025-09-19 19:06:56 +1000

    fix import

--- blotztask-mobile/src/shared/components/common/gradient-circle.tsx
+++ blotztask-mobile/src/shared/components/common/gradient-circle.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pressable, ColorValue } from "react-native";
+import { ColorValue } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 type GradientColors = Readonly<[ColorValue, ColorValue, ...ColorValue[]]>;
