a5482ebf66b3d2bc9a7b7d0dcff0975e9426a9ef
nicole-nxn <nicolenxn1234@gmail.com>
2025-09-22 11:02:41 +1000

    fix error

--- blotztask-mobile/src/shared/components/ui/render-bottomsheet-backdrop.tsx
+++ blotztask-mobile/src/shared/components/ui/render-bottomsheet-backdrop.tsx
@@ -1,15 +1,11 @@
 import { BottomSheetBackdrop, BottomSheetBackdropProps } from "@gorhom/bottom-sheet";
-import { useCallback } from "react";
 
-export const renderBottomSheetBackdrop = useCallback(
-  (props: BottomSheetBackdropProps) => (
-    <BottomSheetBackdrop
-      {...props}
-      appearsOnIndex={0}
-      disappearsOnIndex={-1}
-      pressBehavior="close"
-      opacity={0.5}
-    />
-  ),
-  [],
+export const renderBottomSheetBackdrop = (props: BottomSheetBackdropProps) => (
+  <BottomSheetBackdrop
+    {...props}
+    appearsOnIndex={0}
+    disappearsOnIndex={-1}
+    pressBehavior="close"
+    opacity={0.5}
+  />
 );
