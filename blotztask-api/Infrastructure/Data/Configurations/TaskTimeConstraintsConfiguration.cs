466cef4f75ad6ea70c68d42ab8da7397ba08530c
JoeLi <liyuanzhuo0704@gmail.com>
2025-09-27 23:13:26 +1000

    refineDomainConstraints

--- blotztask-api/Infrastructure/Data/Configurations/TaskTimeConstraintsConfiguration.cs
+++ blotztask-api/Infrastructure/Data/Configurations/TaskTimeConstraintsConfiguration.cs
@@ -12,12 +12,18 @@ public static class TaskTimeConstraintsConfiguration
 
         builder.ToTable(t =>
         {
-            t.HasCheckConstraint($"CK_{tableName}_TimeType_Valid",
-                "([TimeType] IN (0,1) AND ([StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)) OR ([StartTime] IS NULL AND [EndTime] IS NULL)");
-            t.HasCheckConstraint($"CK_{tableName}_SingleTimeValidation", "([TimeType] <> 0) OR ([StartTime] = [EndTime])");
-            t.HasCheckConstraint($"CK_{tableName}_TimeRangeValidation",
-                "([StartTime] IS NULL AND [EndTime] IS NULL) OR " +
-                "([StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)");
+            t.HasCheckConstraint($"CK_{tableName}_Time_Presence",
+                "(" +
+                "  ([TimeType] IS NULL AND [StartTime] IS NULL AND [EndTime] IS NULL)" +
+                "   OR" +
+                "  ([TimeType] IN (0,1) AND [StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)" +
+                ")");
+
+            t.HasCheckConstraint($"CK_{tableName}_SingleTime_Equals",
+                "([TimeType] IS NULL) OR ([TimeType] <> 0) OR ([StartTime] = [EndTime])");
+
+            t.HasCheckConstraint($"CK_{tableName}_Start_Before_Or_Equal_End",
+                "([StartTime] IS NULL AND [EndTime] IS NULL) OR ([StartTime] <= [EndTime])");
         });
     }
 }
