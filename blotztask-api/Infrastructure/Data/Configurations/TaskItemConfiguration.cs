3dc50ad3d4f9268fe6338a4faadc6c5bccd5cf2d
JoeLi <liyuanzhuo0704@gmail.com>
2025-09-28 14:43:27 +1000

    time type constraint migration

--- blotztask-api/Infrastructure/Data/Configurations/TaskItemConfiguration.cs
+++ blotztask-api/Infrastructure/Data/Configurations/TaskItemConfiguration.cs
@@ -8,20 +8,20 @@ public class TaskItemConfiguration : IEntityTypeConfiguration<TaskItem>
 {
     public void Configure(EntityTypeBuilder<TaskItem> builder)
     {
-        //builder.ToTable(t =>
-        //{
-        //    t.HasCheckConstraint($"CK_TaskItem_Time_Valid",
-        //        "(" +
-        //        "  ([TimeType] IS NULL AND [StartTime] IS NULL AND [EndTime] IS NULL)" +
-        //        "   OR" +
-        //        "  ([TimeType] IN NOT NULL AND [StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)" +
-        //        ")");
+        builder.ToTable(t =>
+        {
+            t.HasCheckConstraint($"CK_TaskItem_Time_Valid",
+                "(" +
+                "  ([TimeType] IS NULL AND [StartTime] IS NULL AND [EndTime] IS NULL)" +
+                "   OR" +
+                "  ([TimeType] IS NOT NULL AND [StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)" +
+                ")");
 
-        //    t.HasCheckConstraint($"CK_TaskItem_SingleTime_Equals",
-        //        "([TimeType] IS NULL) OR ([TimeType] <> 0) OR ([StartTime] = [EndTime])");
+            t.HasCheckConstraint($"CK_TaskItem_SingleTime_Equals",
+                "([TimeType] IS NULL) OR ([TimeType] <> 0) OR ([StartTime] = [EndTime])");
 
-        //    t.HasCheckConstraint($"CK_TaskItem_Start_Before_Or_Equal_End",
-        //        "([StartTime] IS NULL AND [EndTime] IS NULL) OR ([StartTime] <= [EndTime])");
-        //});
+            t.HasCheckConstraint($"CK_TaskItem_Start_Before_Or_Equal_End",
+                "([StartTime] IS NULL AND [EndTime] IS NULL) OR ([StartTime] <= [EndTime])");
+        });
     }
 }
