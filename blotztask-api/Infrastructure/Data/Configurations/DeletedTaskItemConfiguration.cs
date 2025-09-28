402334e37f67e0521fc1c9e507378ca7c42c7713
JoeLi <liyuanzhuo0704@gmail.com>
2025-09-28 14:39:02 +1000

    add time type migration

--- blotztask-api/Infrastructure/Data/Configurations/DeletedTaskItemConfiguration.cs
+++ blotztask-api/Infrastructure/Data/Configurations/DeletedTaskItemConfiguration.cs
@@ -8,20 +8,20 @@ public class DeletedTaskItemConfiguration : IEntityTypeConfiguration<DeletedTask
 {
     public void Configure(EntityTypeBuilder<DeletedTaskItem> builder)
     {
-        builder.ToTable(t =>
-        {
-            t.HasCheckConstraint($"CK_DeletedTaskItem_Time_Valid",
-                "(" +
-                "  ([TimeType] IS NULL AND [StartTime] IS NULL AND [EndTime] IS NULL)" +
-                "   OR" +
-                "  ([TimeType] IN NOT NULL AND [StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)" +
-                ")");
+        //builder.ToTable(t =>
+        //{
+        //    t.HasCheckConstraint($"CK_DeletedTaskItem_Time_Valid",
+        //        "(" +
+        //        "  ([TimeType] IS NULL AND [StartTime] IS NULL AND [EndTime] IS NULL)" +
+        //        "   OR" +
+        //        "  ([TimeType] IN NOT NULL AND [StartTime] IS NOT NULL AND [EndTime] IS NOT NULL)" +
+        //        ")");
 
-            t.HasCheckConstraint($"CK_DeletedTaskItem_SingleTime_Equals",
-                "([TimeType] IS NULL) OR ([TimeType] <> 0) OR ([StartTime] = [EndTime])");
+        //    t.HasCheckConstraint($"CK_DeletedTaskItem_SingleTime_Equals",
+        //        "([TimeType] IS NULL) OR ([TimeType] <> 0) OR ([StartTime] = [EndTime])");
 
-            t.HasCheckConstraint($"CK_DeletedTaskItem_Start_Before_Or_Equal_End",
-                "([StartTime] IS NULL AND [EndTime] IS NULL) OR ([StartTime] <= [EndTime])");
-        });
+        //    t.HasCheckConstraint($"CK_DeletedTaskItem_Start_Before_Or_Equal_End",
+        //        "([StartTime] IS NULL AND [EndTime] IS NULL) OR ([StartTime] <= [EndTime])");
+        //});
     }
 }
