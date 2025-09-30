3e9e34dd9e91e85d6ba919b1fca3a5bb152e3efb
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-10-01 00:46:40 +1000

    Support custom label and add constraints (#527)

---
+++ blotztask-api/Infrastructure/Data/Configurations/LabelConfiguration.cs
@@ -0,0 +1,20 @@
+using BlotzTask.Modules.Labels.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlotzTask.Infrastructure.Data.Configurations;
+
+public class LabelConfiguration : IEntityTypeConfiguration<Label>
+{
+    public void Configure(EntityTypeBuilder<Label> builder)
+    {
+        builder.Property(l => l.Scope)
+            .HasConversion<string>();
+
+        builder.ToTable("Labels", t =>
+        {
+            t.HasCheckConstraint("CK_Label_Scope_User",
+                "(Scope = 'Global' AND UserId IS NULL) OR (Scope = 'Custom' AND UserId IS NOT NULL)");
+        });
+    }
+}
---
+++ blotztask-api/Modules/Labels/Enums/LabelScope.cs
@@ -0,0 +1,8 @@
+namespace BlotzTask.Modules.Labels.Enums
+{
+    public enum LabelScope
+    {
+        Global = 0,
+        Custom = 1,
+    }
+}
