70640e0fec2b0f779b0bb84a7976d79a9ca38343
JoeLi <137287434+Phenomenax@users.noreply.github.com>
2025-10-01 00:46:40 +1000

    Support custom label and add constraints (#527)

--- blotztask-api/Modules/Labels/Commands/Addlabel.cs
+++ blotztask-api/Modules/Labels/Commands/Addlabel.cs
@@ -1,6 +1,7 @@
-using System.ComponentModel.DataAnnotations;
 using BlotzTask.Infrastructure.Data;
 using BlotzTask.Modules.Labels.Domain;
+using BlotzTask.Modules.Labels.Enums;
+using System.ComponentModel.DataAnnotations;
 namespace BlotzTask.Modules.Labels.Commands;
 
 public class AddLabelCommand
@@ -9,21 +10,24 @@ public class AddLabelCommand
     public string Name { get; set; } 
     [Required]
     public string Color { get; set; } 
-    public string Description { get; set; } 
-    
+    public string Description { get; set; }
+    public LabelScope Scope { get; set; }
+    public Guid? UserId { get; set; }
 }
 
 public class AddLabelCommandHandler(BlotzTaskDbContext db, ILogger<AddLabelCommandHandler> logger)
 {
     public async Task<String> Handle(AddLabelCommand command, CancellationToken ct = default )
     {
-        logger.LogInformation("Adding label {Name}", command.Name);
-        
+        logger.LogInformation("Adding {Scope} label {Name}", command.Scope, command.Name);
+
         var addlabel = new Label
         {
             Name = command.Name,
             Color = command.Color,
-            Description = command.Description
+            Description = command.Description,
+            Scope = command.Scope,
+            UserId = command.UserId
         };
 
         db.Labels.Add(addlabel);
