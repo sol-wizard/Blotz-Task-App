c82ff021b4202c054f4195b84b692c96c2bdd22b
JoeLi <liyuanzhuo0704@gmail.com>
2025-10-01 20:32:15 +1000

    fix default value check

--- blotztask-mobile/src/feature/task/components/form/label-select.tsx
+++ blotztask-mobile/src/feature/task/components/form/label-select.tsx
@@ -48,7 +48,7 @@ export function LabelSelect({ control }: { control: any }) {
             >
               <Menu.Item
                 onPress={() => {
-                  onChange(null);
+                  onChange(undefined);
                   setVisible(false);
                 }}
                 title="No Label"
