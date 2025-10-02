72d43a9e17f951a859801e35ff7e43fc66baabe2
nicole-nxn <nicolenxn1234@gmail.com>
2025-10-02 18:18:08 +1000

    make add task from ai works

--- blotztask-mobile/src/feature/ai-chat-hub/util/ai-task-generator-util.ts
+++ blotztask-mobile/src/feature/ai-chat-hub/util/ai-task-generator-util.ts
@@ -2,12 +2,22 @@ import { AddTaskItemDTO } from "@/shared/models/add-task-item-dto";
 import { AiTaskDTO } from "../models/ai-task-dto";
 
 export function convertAiTaskToAddTaskItemDTO(task: AiTaskDTO): AddTaskItemDTO {
+  if (!task.startTime || !task.endTime)
+    return {
+      title: task.title,
+      description: task.description,
+      labelId: task.labelId,
+    };
+  let timeType = 1;
+  if (task.startTime === task.endTime) {
+    timeType = 0;
+  }
   return {
     title: task.title,
     description: task.description,
     startTime: new Date(task.startTime),
     endTime: new Date(task.endTime),
     labelId: task.labelId,
-    timeType: 1,
+    timeType: timeType,
   };
 }
