7774d91a5207e96d0cb7a405077e6bb184227cd7
Winn Z <winnwyz@gmail.com>
2025-10-03 12:53:45 +1000

    Show calendar when selecting SelectDate

--- blotztask-mobile/src/feature/task-add-edit/components/date-section/date-select-single-day.tsx
+++ blotztask-mobile/src/feature/task-add-edit/components/date-section/date-select-single-day.tsx
@@ -1,17 +1,31 @@
 import { View, Text } from "react-native";
 import React, { useState } from "react";
 import { RadioButton } from "react-native-paper";
+import CalendarDatePicker from "./calendar-date-picker";
 
 const DateSelectSingleDay = () => {
   const [selected, setSelected] = useState("Today");
+  const [showCalendar, setShowCalendar] = useState(false);
 
   return (
-    <RadioButton.Group onValueChange={setSelected} value={selected}>
+    <RadioButton.Group
+      onValueChange={(value) => {
+        setSelected(value);
+        if (value === "Select Date") {
+          setShowCalendar(true);
+        } else {
+          setShowCalendar(false);
+        }
+      }}
+      value={selected}
+    >
       <View className="flex-row justify-start gap-12">
         {["Today", "Tomorrow"].map((option) => (
           <View key={option} className="flex-row items-center gap-2">
             <View
-              className={`rounded-full p-1 flex justify-center items-center ${selected === option ? "bg-blue-100" : "bg-white"}`}
+              className={`rounded-full p-1 flex justify-center items-center ${
+                selected === option ? "bg-blue-100" : "bg-white"
+              }`}
               style={{ transform: [{ scale: 0.5 }] }}
             >
               <RadioButton value={option} color="#B0D0FA" />
@@ -23,13 +37,21 @@ const DateSelectSingleDay = () => {
 
       <View className="flex-row justify-start items-center gap-2 ">
         <View
-          className={`rounded-full p-1 flex justify-center items-center ${selected === "Select Date" ? "bg-blue-100" : "bg-white"}`}
+          className={`rounded-full p-1 flex justify-center items-center ${
+            selected === "Select Date" ? "bg-blue-100" : "bg-white"
+          }`}
           style={{ transform: [{ scale: 0.5 }] }}
         >
           <RadioButton value="Select Date" color="#B0D0FA" />
         </View>
         <Text className="text-lg">Select Date</Text>
       </View>
+
+      {showCalendar && (
+        <View>
+          <CalendarDatePicker />
+        </View>
+      )}
     </RadioButton.Group>
   );
 };
