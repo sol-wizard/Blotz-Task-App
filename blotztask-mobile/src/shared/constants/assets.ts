8d3e57cad431e9aa5c4bbae66c978abb62731764
Winn Z <winnwyz@gmail.com>
2025-10-07 17:36:30 +1100

    Remove unused file import

--- blotztask-mobile/src/shared/constants/assets.ts
+++ blotztask-mobile/src/shared/constants/assets.ts
@@ -9,7 +9,6 @@ export const LOTTIE_ANIMATIONS = {
   aiLoadingEffect: require("../../../assets/animations/aiLoadingEffect.json"),
   spinner: require("../../../assets/animations/spinner.json"),
   voiceWave: require("../../../assets/animations/voice-wave.json"),
-  searchMicWave: require("../../../assets/animations/search-mic-wave.json"),
 } as const;
 
 // Images
