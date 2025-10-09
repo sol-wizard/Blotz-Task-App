c0122d9097d7127b5f32c30421f1b91dd929ae98
Nicole <nicolenxn1234@gmail.com>
2025-10-09 15:39:53 +1100

    refactor ai backend (#546)

--- blotztask-mobile/src/feature/ai-task-generate/component/voice-input.tsx
+++ blotztask-mobile/src/feature/ai-task-generate/component/voice-input.tsx
@@ -7,17 +7,16 @@ import { ASSETS } from "@/shared/constants/assets";
 
 export const VoiceInput = ({
   hasError,
-  text,
-  setText,
   sendMessage,
   setInputError,
+  errorMessage,
 }: {
   hasError: boolean;
-  text: string;
-  setText: (value: string) => void;
   sendMessage: (v: string) => void;
   setInputError: (v: boolean) => void;
+  errorMessage?: string;
 }) => {
+  const [text, setText] = useState("");
   const [language, setLanguage] = useState<"en" | "zh">("zh");
   const { startListening, partialText, stopAndGetText, isListening } = useVoiceInput({ language });
   const displayText = isListening
@@ -44,9 +43,7 @@ export const VoiceInput = ({
           className="bg-background rounded-2xl py-6 px-4 flex-row w-96"
           style={{ minHeight: idleBlockH }}
         >
-          <Text className="text-[#3D8DE0] text-2xl font-balooBold pt-2 w-72">
-            Oops, that went over my head. Can you say it again
-          </Text>
+          <Text className="text-[#3D8DE0] text-2xl font-balooBold pt-2 w-72">{errorMessage}</Text>
           <Image source={ASSETS.greenBun} className="w-20 h-20" />
         </View>
       )}
