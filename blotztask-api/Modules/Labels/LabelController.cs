8a2f7f6193d4e73c3b02c1dd9ab7f76823e6b177
Rachel <43347454+RachelYrt@users.noreply.github.com>
2025-10-11 15:18:14 +1100

    Add delete custom label feature in label module (#549)
    Co-authored-by: JoeLi <137287434+Phenomenax@users.noreply.github.com>

--- blotztask-api/Modules/Labels/LabelController.cs
+++ blotztask-api/Modules/Labels/LabelController.cs
@@ -1,5 +1,6 @@
-﻿using BlotzTask.Modules.Labels.DTOs;
+using BlotzTask.Modules.Labels.DTOs;
 using BlotzTask.Modules.Labels.Queries;
+using BlotzTask.Modules.Labels.Commands;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,8 +11,10 @@ namespace BlotzTask.Modules.Labels;
 [Route("api/[controller]")]
 public class LabelController(
     GetAllLabelsQueryHandler getAllLabelsQueryHandler,
-    GetLabelTaskCountQueryHandler getLabelTaskCountQueryHandler) : ControllerBase
+    GetLabelTaskCountQueryHandler getLabelTaskCountQueryHandler,
+    DeleteCustomLabelCommandHandler deleteCustomLabelCommandHandler) : ControllerBase
 {
+
 
     [HttpGet]
     public async Task<List<LabelDTO>> GetAllLabels(CancellationToken ct)
@@ -28,5 +31,18 @@ public class LabelController(
     {
         var query = new GetLabelTaskCountQuery { LabelId = labelId };
         return await getLabelTaskCountQueryHandler.Handle(query, ct);
+    }
+    [HttpDelete("{id}")]
+    public async Task<string> DeleteCustomLabel(int id, CancellationToken ct)
+    {
+        if (!HttpContext.Items.TryGetValue("UserId", out var userIdObj) || userIdObj is not Guid userId)
+            throw new UnauthorizedAccessException("Could not find valid user id from Http Context");
+        var command = new DeleteCustomLabelCommand
+        {
+            LabelId = id,
+            UserId = userId
+        };
+        return await deleteCustomLabelCommandHandler.Handle(command, ct);
+
     }
 }
