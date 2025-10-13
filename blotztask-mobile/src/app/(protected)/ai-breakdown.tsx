cad42bf2113507be53b48deb8c9900c0bf865a5a
Winn Z <winnwyz@gmail.com>
2025-10-13 22:12:53 +1100

    Delete breakdown chathub in frontend

--- blotztask-mobile/src/app/(protected)/ai-breakdown.tsx
+++ blotztask-mobile/src/app/(protected)/ai-breakdown.tsx
@@ -1,6 +1,5 @@
-import { useBreakdownChat } from "@/feature/breakdown/hooks/useBreakdownChat";
 import { router, useLocalSearchParams } from "expo-router";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   View,
   Text,
@@ -11,41 +10,62 @@ import {
   Keyboard,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import BreakdownBotMessage from "@/feature/breakdown/components/breakdown-bot-message";
 import { AddSubtaskBottomSheet } from "@/feature/breakdown/components/add-subtask-bottom-sheet";
 import { AddSubtaskDTO } from "@/feature/breakdown/models/add-subtask-dto";
-import { addSubtasks } from "@/shared/services/subtask-service";
+import { addSubtasks, createBreakDownSubtasks } from "@/shared/services/subtask-service";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { BreakdownSubtaskDTO } from "@/feature/breakdown/models/breakdown-subtask-dto";
 
 export default function AiBreakdownScreen() {
   const { id: taskId } = useLocalSearchParams<{ id?: string }>();
   if (!taskId) throw new Error("Missing task id");
 
-  const [selectedSubtasks, setSelectedSubtasks] = useState<AddSubtaskDTO[]>([]);
+  const [selectedSubtasks] = useState<AddSubtaskDTO[]>([]);
   const addSubtaskSheetRef = useRef<BottomSheetModal>(null);
 
-  const { messages, isTyping } = useBreakdownChat(taskId);
+  const [subtasks, setSubtasks] = useState<BreakdownSubtaskDTO[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  const handleSelectSubtask = (subTask: AddSubtaskDTO) => {
-    setSelectedSubtasks((prev) => {
-      const exists = prev.some((st) => st.title === subTask.title);
+  useEffect(() => {
+    const fetchSubtasks = async () => {
+      setLoading(true);
+      const data = await createBreakDownSubtasks(Number(taskId));
+      setSubtasks(data || []);
+      setLoading(false);
+    };
 
-      let next: AddSubtaskDTO[];
-      if (exists) {
-        next = prev.filter((st) => st.title !== subTask.title);
-      } else {
-        next = [...prev, subTask];
-      }
+    fetchSubtasks();
+  }, [taskId]);
 
-      if (next.length > 0) {
-        addSubtaskSheetRef.current?.present();
-      } else {
-        addSubtaskSheetRef.current?.dismiss?.();
-      }
+  if (loading)
+    return (
+      <View className="mb-4">
+        <View className="bg-gray-100 p-3 rounded-lg mr-12">
+          <Text className="text-gray-500">Bot is thinking...</Text>
+        </View>
+      </View>
+    );
 
-      return next;
-    });
-  };
+  // const handleSelectSubtask = (subTask: AddSubtaskDTO) => {
+  //   setSelectedSubtasks((prev) => {
+  //     const exists = prev.some((st) => st.title === subTask.title);
+
+  //     let next: AddSubtaskDTO[];
+  //     if (exists) {
+  //       next = prev.filter((st) => st.title !== subTask.title);
+  //     } else {
+  //       next = [...prev, subTask];
+  //     }
+
+  //     if (next.length > 0) {
+  //       addSubtaskSheetRef.current?.present();
+  //     } else {
+  //       addSubtaskSheetRef.current?.dismiss?.();
+  //     }
+
+  //     return next;
+  //   });
+  // };
 
   const handleAddSubtasks = async () => {
     try {
@@ -70,26 +90,9 @@ export default function AiBreakdownScreen() {
               contentContainerStyle={{ flexGrow: 1, paddingBottom: 100 }}
               keyboardShouldPersistTaps="handled"
             >
-              {messages &&
-                messages.map(
-                  (msg, index) =>
-                    msg.isBot ?? (
-                      <BreakdownBotMessage
-                        key={index}
-                        parentTaskId={taskId as string}
-                        text={msg.content}
-                        subtasks={msg.subtasks}
-                        openAddSubtaskBottomSheet={handleSelectSubtask}
-                      />
-                    ),
-                )}
-              {isTyping && (
-                <View className="mb-4">
-                  <View className="bg-gray-100 p-3 rounded-lg mr-12">
-                    <Text className="text-gray-500">Bot is thinking...</Text>
-                  </View>
-                </View>
-              )}
+              {subtasks.map((subtask) => (
+                <Text key={subtask.title}>{subtask.title}</Text>
+              ))}
             </ScrollView>
           </TouchableWithoutFeedback>
 
