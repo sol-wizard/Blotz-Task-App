8fd3bb8bd7984ddba12bafca0a6e2728479bd13d
Ethan <guotaixiao87@gmail.com>
2025-10-14 11:02:51 +1100

    Rewrite taskbreakdown (#557)

--- blotztask-mobile/src/feature/breakdown/models/breakdown-message.ts
+++
@@ -1,8 +0,0 @@
-import { BreakdownSubtaskDTO } from "./breakdown-subtask-dto";
-
-// Message structure for breakdown chat
-export interface BreakdownMessage {
-  content: string;
-  isBot: boolean;
-  subtasks?: BreakdownSubtaskDTO[];
-}
