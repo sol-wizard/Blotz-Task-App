b2882c3c6b5e6463337d8fd2347beb941dc071e7
Nicole <nicolenxn1234@gmail.com>
2025-10-16 18:11:23 +1100

    Refactor form based on new desgin (#556)

--- blotztask-mobile/src/feature/task-add-edit/task-form.tsx
+++ blotztask-mobile/src/feature/task-add-edit/task-form.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { View, Text, Pressable, ScrollView } from "react-native";
-import { FormProvider, useForm, useWatch } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TaskFormField, taskFormSchema } from "./models/task-form-schema";
 import { EditTaskItemDTO } from "./models/edit-task-item-dto";
-import DateSection from "./components/date-section/date-section";
 import { FormTextInput } from "@/shared/components/ui/form-text-input";
 import { LabelSelect } from "./components/label-select";
-import { FormDivider } from "./components/form-divider";
-import TimeSection from "./components/time-sections/time-section";
-import { isMultiDay, isSingleDay } from "./util/date-time-helpers";
+import { FormDivider } from "../../shared/components/ui/form-divider";
+
+import { ReminderTab } from "./components/reminder-tab";
+import { EventTab } from "./components/event-tab";
+import { SegmentButton } from "./components/segment-button";
+import { isEqual } from "date-fns";
+import { combineDateTime } from "./util/combine-date-time";
+import { SegmentButtonValue } from "./models/segment-button-value";
 
 type TaskFormProps =
   | {
@@ -24,123 +28,83 @@ type TaskFormProps =
     };
 
 const TaskForm = ({ mode, dto, onSubmit }: TaskFormProps) => {
+  const defaultValues: TaskFormField = {
+    title: dto?.title ?? "",
+    description: dto?.description ?? "",
+    labelId: dto?.labelId ?? null,
+    startDate: dto?.startTime ?? null,
+    startTime: dto?.startTime ?? null,
+    endDate: dto?.endTime ?? null,
+    endTime: dto?.endTime ?? null,
+  };
   const form = useForm<TaskFormField>({
     resolver: zodResolver(taskFormSchema),
     mode: "onChange",
-    defaultValues: {
-      title: dto?.title ?? "",
-      description: dto?.description ?? "",
-      labelId: dto?.labelId ?? null,
-      startDate: dto?.startTime ?? null,
-      startTime: dto?.startTime ?? null,
-      endDate: dto?.endTime ?? null,
-      endTime: dto?.endTime ?? null,
-    },
+    defaultValues: defaultValues,
   });
 
   const { handleSubmit, formState, control, setValue } = form;
   const { isValid, isSubmitting } = formState;
 
-  const [isFloatingTask, setIsFloatingTask] = useState(() => {
-    if (mode === "create") return false; // Default to non-floating task creation
-    return dto.timeType === null; // Floating if editing task timeType is null
-  });
+  const startCombined = combineDateTime(defaultValues.startDate, defaultValues.startTime);
+  const endCombined = combineDateTime(defaultValues.endDate, defaultValues.endTime);
+  const initialTab: SegmentButtonValue =
+    !startCombined || !endCombined || isEqual(startCombined, endCombined) ? "reminder" : "event";
 
-  // When submitting, clear dates/times if floating
+  const [isActiveTab, setIsActiveTab] = useState<SegmentButtonValue>(initialTab);
+
   const handleFormSubmit = (data: TaskFormField) => {
-    if (isFloatingTask) {
-      onSubmit({
-        ...data,
-        startDate: null,
-        startTime: null,
-        endDate: null,
-        endTime: null,
-      });
-    } else {
-      onSubmit(data);
-    }
+    onSubmit(data);
   };
 
-  const startDate = useWatch({ control, name: "startDate" });
-  const endDate = useWatch({ control, name: "endDate" });
-  const isMultiDayTask = isMultiDay(startDate, endDate);
-  const isSingleDayTask = isSingleDay(startDate, endDate);
+  const handleTabChange = (next: SegmentButtonValue) => {
+    setIsActiveTab(next);
+
+    setValue("startDate", null);
+    setValue("startTime", null);
+    setValue("endDate", null);
+    setValue("endTime", null);
+  };
 
   return (
-    <FormProvider {...form}>
-      <View className="flex-1 relative">
+    <View className="flex-1 bg-white">
+      <FormProvider {...form}>
         <ScrollView className="flex-col py-6 px-8" contentContainerStyle={{ paddingBottom: 100 }}>
           {/* Title */}
-          <View className="mb-8 bg-white">
+          <View className="mb-4 bg-white">
             <FormTextInput
               name="title"
-              placeholder="Title"
+              placeholder="New Task"
               control={control}
-              className="font-balooBold text-5xl leading-normal p-2  border border-gray-300 "
+              className="font-balooBold text-5xl leading-normal"
               inputProps={{
                 multiline: false,
               }}
             />
           </View>
 
-          {/* Category */}
+          <View className="mb-8 py-3 bg-background rounded-2xl px-4">
+            <FormTextInput
+              name="description"
+              placeholder="Add a note"
+              control={control}
+              className="font-baloo text-lg text-tertiary"
+            />
+          </View>
+
+          <FormDivider />
+          <SegmentButton value={isActiveTab} setValue={handleTabChange} />
+
+          {isActiveTab === "reminder" && <ReminderTab control={control} />}
+          {isActiveTab === "event" && <EventTab control={control} />}
+          <FormDivider />
+
           <View className="mb-8">
             <Text className="font-balooBold text-3xl leading-normal">Category</Text>
             <LabelSelect control={control} />
           </View>
 
           <FormDivider />
-
-          {/* Description Section */}
-          {/* TODO: Remove description section for now and waiting for design */}
-          {/* <View className="mb-8">
-            <Text className="font-balooBold text-3xl leading-normal">Description</Text>
-            <FormTextInput
-              name="description"
-              placeholder="Add details about this task (optional)"
-              control={control}
-              className="bg-gray-200 font-baloo text-lg"
-            />
-          </View> */}
-
-          {/* <FormDivider /> */}
-
-          <Pressable
-            onPress={() => setIsFloatingTask((prev) => !prev)}
-            className={`w-full py-3 mb-6 rounded-lg items-center justify-center ${
-              isFloatingTask ? "bg-blue-400" : "bg-lime-300"
-            }`}
-          >
-            <Text className="font-balooBold text-xl text-black">
-              {isFloatingTask ? "No Datetime Task enabled" : "Enable No Datetime Task"}
-            </Text>
-          </Pressable>
-
-          <FormDivider />
-
-          {!isFloatingTask && (
-            <>
-              {/* Date Section */}
-              <DateSection
-                control={control}
-                setValue={setValue}
-                dto={dto}
-                startDate={startDate} // pass watched value
-                endDate={endDate} // pass watched value
-              />
-
-              <FormDivider />
-
-              {/* Time Section */}
-              <TimeSection
-                control={control}
-                setValue={setValue}
-                dto={dto}
-                isMultiDayTask={isMultiDayTask}
-                isSingleDayTask={isSingleDayTask}
-              />
-            </>
-          )}
         </ScrollView>
 
         {/* Submit */}
@@ -157,8 +121,8 @@ const TaskForm = ({ mode, dto, onSubmit }: TaskFormProps) => {
             </Text>
           </Pressable>
         </View>
-      </View>
-    </FormProvider>
+      </FormProvider>
+    </View>
   );
 };
 
