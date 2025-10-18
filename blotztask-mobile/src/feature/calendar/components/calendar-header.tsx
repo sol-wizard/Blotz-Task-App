97e1e351e1eaa97dea6c53a43b575b766393110a
Li CY <104789004+LiChenyang-GZ@users.noreply.github.com>
2025-10-18 15:15:40 +1100

    add SafeAreaView (#564)
    Co-authored-by: nicole-nxn <nicolenxn1234@gmail.com>

--- blotztask-mobile/src/feature/calendar/components/calendar-header.tsx
+++ blotztask-mobile/src/feature/calendar/components/calendar-header.tsx
@@ -19,23 +19,23 @@ export default function CalendarHeader({
   return (
     <View className="flex-row items-center justify-between px-5">
       <View className="flex-row items-center gap-3">
-      <Text className="text-5xl font-bold text-gray-800 font-balooExtraBold items-end pt-10">
-        {dayOfWeek}
-      </Text>
-     
-      <TouchableOpacity onPress={onToggleCalendar} className="pt-5" activeOpacity={0.7}>
-        <Ionicons
-          name={isCalendarVisible ? "chevron-up" : "chevron-down"}
-          size={22}
-          className="text-gray-800"
-          style={{
-            fontWeight: "800",
-            textShadowColor: "#1F2937",
-            textShadowOffset: { width: 0.7, height: 0.7 },
-            textShadowRadius: 0.7,
-          }}
-        />
-      </TouchableOpacity>
+        <Text className="text-5xl font-bold text-gray-800 font-balooExtraBold items-end pt-10">
+          {dayOfWeek}
+        </Text>
+
+        <TouchableOpacity onPress={onToggleCalendar} className="pt-5" activeOpacity={0.7}>
+          <Ionicons
+            name={isCalendarVisible ? "chevron-up" : "chevron-down"}
+            size={22}
+            className="text-gray-800"
+            style={{
+              fontWeight: "800",
+              textShadowColor: "#1F2937",
+              textShadowOffset: { width: 0.7, height: 0.7 },
+              textShadowRadius: 0.7,
+            }}
+          />
+        </TouchableOpacity>
       </View>
 
       <UserProfile />
