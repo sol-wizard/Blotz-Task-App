4d3ec8073f5dee5637b0f06b15b3c56355e55a0b
Winn Z <winnwyz@gmail.com>
2025-10-21 14:30:48 +1100

    Make use of semantic kernel for structured output and future function expand

--- blotztask-api/Modules/BreakDown/DependencyInjection.cs
+++ blotztask-api/Modules/BreakDown/DependencyInjection.cs
@@ -1,11 +1,13 @@
 using BlotzTask.Modules.BreakDown.Commands;
+using BlotzTask.Modules.BreakDown.Services;
 
 namespace BlotzTask.Modules.BreakDown;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection  AddTaskBreakdownModule(this IServiceCollection services)
+    public static IServiceCollection AddTaskBreakdownModule(this IServiceCollection services)
     {
+        services.AddScoped<ITaskBreakdownService, TaskBreakdownService>();
         services.AddScoped<BreakdownTaskCommandHandler>();
 
         return services;
