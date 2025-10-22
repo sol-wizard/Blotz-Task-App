8d069fa9223f833ccd8a109db7cad2ebb04de900
WINN <winnwyz@gmail.com>
2025-10-22 14:47:42 +1100

    refactor task breakdown using semantic kernel (#569)

--- blotztask-api/Modules/BreakDown/DependencyInjection.cs
+++ blotztask-api/Modules/BreakDown/DependencyInjection.cs
@@ -4,7 +4,7 @@ namespace BlotzTask.Modules.BreakDown;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection  AddTaskBreakdownModule(this IServiceCollection services)
+    public static IServiceCollection AddTaskBreakdownModule(this IServiceCollection services)
     {
         services.AddScoped<BreakdownTaskCommandHandler>();
 
