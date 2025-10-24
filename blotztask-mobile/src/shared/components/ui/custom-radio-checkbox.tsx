060aded3119b6ce3319bd3f87e8ed9970efda414
Winn Z <winnwyz@gmail.com>
2025-10-24 15:06:44 +1100

    Apply default color to checkbox

--- blotztask-mobile/src/shared/components/ui/custom-radio-checkbox.tsx
+++ blotztask-mobile/src/shared/components/ui/custom-radio-checkbox.tsx
@@ -7,21 +7,19 @@ type CustomRadioCheckboxProps = {
   color?: string;
 };
 
-export const CustomRadioCheckbox = ({ checked, onPress, color }: CustomRadioCheckboxProps) => {
-  const activeColor = color ?? theme.colors.disabled;
-
+export const CustomRadioCheckbox = ({ checked, onPress, color = theme.colors.disabled }: CustomRadioCheckboxProps) => {
   return (
     <Pressable
       className="w-6 h-6 rounded-full border-2 mr-4 items-center justify-center"
       style={{
-        borderColor: checked ? activeColor : theme.colors.disabled,
+        borderColor: checked ? color : theme.colors.disabled,
       }}
       onPress={onPress}
     >
       {checked && (
         <View
           className="w-3 h-3 rounded-full"
-          style={{ backgroundColor: activeColor }}
+          style={{ backgroundColor: color }}
         />
       )}
     </Pressable>
