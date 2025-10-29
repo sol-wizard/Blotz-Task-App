b7e32ce6ee035e2345c8e22ab7129e5f41907584
Nicole <nicolenxn1234@gmail.com>
2025-10-29 11:20:45 +1100

    Set up gashapon machine (#4)

---
+++ blotztask-mobile/src/feature/gashapon-machine/components/machine-button.tsx
@@ -0,0 +1,114 @@
+import React, { useCallback } from "react";
+import { Image } from "react-native";
+import Animated, { useSharedValue, useAnimatedStyle, runOnJS } from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { ASSETS } from "@/shared/constants/assets";
+
+export const MachineButton = ({
+  size = 120,
+  onRelease,
+}: {
+  size?: number;
+  onRelease: (deltaThisTurn: number, newTotal: number) => void;
+}) => {
+  const totalRotation = useSharedValue(0);
+
+  const dragRotation = useSharedValue(0);
+
+  const startTotalRotation = useSharedValue(0);
+  const prevRawAngle = useSharedValue(0);
+
+  const unwrappedFingerAngle = useSharedValue(0);
+
+  const startUnwrappedAngle = useSharedValue(0);
+
+  const handleReleaseOnJS = useCallback(
+    (deltaThisTurn: number, newTotal: number) => {
+      onRelease?.(deltaThisTurn, newTotal);
+    },
+    [onRelease],
+  );
+
+  const panGesture = Gesture.Pan()
+    .onStart((e) => {
+      const centerX = size / 2;
+      const centerY = size / 2;
+
+      const dx = e.x - centerX;
+      const dy = e.y - centerY;
+
+      const angleRad = Math.atan2(dy, dx);
+      const angleDeg = (angleRad * 180) / Math.PI;
+
+      prevRawAngle.value = angleDeg;
+      unwrappedFingerAngle.value = angleDeg;
+      startUnwrappedAngle.value = angleDeg;
+
+      startTotalRotation.value = totalRotation.value;
+
+      dragRotation.value = 0;
+    })
+    .onUpdate((e) => {
+      const centerX = size / 2;
+      const centerY = size / 2;
+
+      const dx = e.x - centerX;
+      const dy = e.y - centerY;
+
+      const angleRad = Math.atan2(dy, dx);
+      const angleDeg = (angleRad * 180) / Math.PI;
+
+      let diff = angleDeg - prevRawAngle.value;
+
+      if (diff > 180) {
+        diff -= 360;
+      } else if (diff < -180) {
+        diff += 360;
+      }
+
+      unwrappedFingerAngle.value = unwrappedFingerAngle.value + diff;
+
+      prevRawAngle.value = angleDeg;
+
+      const deltaThisTurn = unwrappedFingerAngle.value - startUnwrappedAngle.value;
+
+      dragRotation.value = deltaThisTurn;
+    })
+    .onEnd(() => {
+      const newTotal = startTotalRotation.value + dragRotation.value;
+      const deltaThisTurn = dragRotation.value;
+
+      totalRotation.value = newTotal;
+      dragRotation.value = 0;
+
+      runOnJS(handleReleaseOnJS)(deltaThisTurn, newTotal);
+    });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const currentDeg = totalRotation.value + dragRotation.value;
+    return {
+      transform: [
+        {
+          rotate: `${currentDeg}deg`,
+        },
+      ],
+    };
+  });
+
+  return (
+    <GestureDetector gesture={panGesture}>
+      <Animated.View className="justify-center items-center" style={{ width: size, height: size }}>
+        <Animated.View
+          className="justify-center items-center"
+          style={[{ width: size, height: size }, animatedStyle]}
+        >
+          <Image
+            source={ASSETS.turnKnobButton}
+            style={{ width: size, height: size }}
+            resizeMode="contain"
+          />
+        </Animated.View>
+      </Animated.View>
+    </GestureDetector>
+  );
+};
