80850b5db3819020af5bdac3d666e2aab4eba960
Winn Z <winnwyz@gmail.com>
2025-10-30 14:19:38 +1100

    Add migrations to make the subtask oredr mutable

---
+++ blotztask-api/Infrastructure/Data/Migrations/20251030031532_ChangeSubtaskOrderToMutable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlotzTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSubtaskOrderToMutable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
