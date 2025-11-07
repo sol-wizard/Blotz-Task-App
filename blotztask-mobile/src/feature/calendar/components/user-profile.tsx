51678f71abfc515f3bbaa07667aa49299ca4cbd4
Jinglin Zeng <keirinzeng@gmail.com>
2025-11-07 20:42:08 +1100

    create user profile page UI
    enable navigation from calendar header avatar to user details page；
    display avatar, displayName, email and edit button (UI only)

--- blotztask-mobile/src/feature/calendar/components/user-profile.tsx
+++ blotztask-mobile/src/feature/calendar/components/user-profile.tsx
@@ -1,6 +1,7 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
 import { UserProfileDTO } from "@/shared/models/user-profile-dto";
 import { IMAGES } from "@/shared/constants/assets";
+import { useRouter } from "expo-router";
 
 const DEFAULT_IMAGE_URL = IMAGES.blotzIcon;
 
@@ -11,16 +12,26 @@ export default function UserProfile({ profile }: { profile?: UserProfileDTO }) {
 
   const showPlaceholder = !profile.pictureUrl;
 
+  const router = useRouter();
+
   return (
     <View className="items-center justify-center mr-2">
       {showPlaceholder ? (
         <Image source={DEFAULT_IMAGE_URL} className="w-14 h-14 rounded-full" resizeMode="cover" />
       ) : (
+        <TouchableOpacity
+          onPress={() =>
+            router.push({
+              pathname: "/(protected)/user-details",
+            })
+          }
+        >
         <Image
           source={{ uri: profile.pictureUrl! }}
           className="w-14 h-14 rounded-full"
           resizeMode="cover"
         />
+        </TouchableOpacity>
       )}
     </View>
   );
