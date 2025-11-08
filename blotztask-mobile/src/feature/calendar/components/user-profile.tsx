48144afa01590fdb5457dbfdbf2e7611069a213a
Jinglin Zeng <keirinzeng@gmail.com>
2025-11-08 21:09:16 +1100

    fix: unify avatar touch area so placeholder can navigate to profile page

--- blotztask-mobile/src/feature/calendar/components/user-profile.tsx
+++ blotztask-mobile/src/feature/calendar/components/user-profile.tsx
@@ -13,27 +13,25 @@ export default function UserProfile({ profile }: { profile?: UserProfileDTO }) {
 
   const showPlaceholder = !profile.pictureUrl;
 
-  
-
   return (
     <View className="items-center justify-center mr-2">
-      {showPlaceholder ? (
-        <Image source={DEFAULT_IMAGE_URL} className="w-14 h-14 rounded-full" resizeMode="cover" />
-      ) : (
-        <TouchableOpacity
+      <TouchableOpacity
           onPress={() =>
             router.push({
               pathname: "/(protected)/user-details",
             })
           }
         >
+      {showPlaceholder ? (
+        <Image source={DEFAULT_IMAGE_URL} className="w-14 h-14 rounded-full" resizeMode="cover" />
+      ) : (
         <Image
           source={{ uri: profile.pictureUrl! }}
           className="w-14 h-14 rounded-full"
           resizeMode="cover"
         />
-        </TouchableOpacity>
       )}
+      </TouchableOpacity>
     </View>
   );
 }
