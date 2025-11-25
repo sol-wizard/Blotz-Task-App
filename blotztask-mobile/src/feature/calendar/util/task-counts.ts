82e7eed39a7eeca9e28a93f2349673185ae4e11d
nicole-nxn <nicolenxn1234@gmail.com>
2025-11-25 17:53:03 +1100

    remove

--- blotztask-mobile/src/feature/calendar/util/task-counts.ts
+++ blotztask-mobile/src/feature/calendar/util/task-counts.ts
@@ -9,58 +9,16 @@ export function filterSelectedTask({
   selectedDayTasks: TaskDetailDTO[];
   selectedDay?: Date;
 }): TaskFilterGroup[] {
-  const today = new Date();
-
-  const todoTasks: TaskDetailDTO[] = [];
-  const inProgressTasks: TaskDetailDTO[] = [];
-  const doneTasks: TaskDetailDTO[] = [];
-  const overdueTasks: TaskDetailDTO[] = [];
-  const allTasks: TaskDetailDTO[] = [];
-
-  const isInProgress = (task: TaskDetailDTO) => {
-    if (!task.startTime || !task.endTime) return false;
-    const start = new Date(task.startTime);
-    const end = new Date(task.endTime);
-    return !task.isDone && today >= start && today < end;
-  };
-
-  const isOverdue = (task: TaskDetailDTO) => {
-    if (!task.endTime) return false;
-    return !task.isDone && isBefore(new Date(task.endTime), today);
-  };
-
-  for (const task of selectedDayTasks) {
-    const isTaskTodo = isTodo(task);
-    const isTaskInProgress = isInProgress(task);
-    const isTaskDone = task.isDone;
-    const isTaskOverdue = isOverdue(task);
-
-    if (isTaskTodo) todoTasks.push(task);
-    if (isTaskInProgress) inProgressTasks.push(task);
-    if (isTaskDone) doneTasks.push(task);
-    if (isTaskOverdue) overdueTasks.push(task);
-
-    if (selectedDay) {
-      if (isAfter(selectedDay, today)) {
-        if (isTaskTodo || isTaskInProgress) allTasks.push(task);
-      } else if (isBefore(selectedDay, today)) {
-        if (isTaskTodo || isTaskDone || isTaskOverdue) allTasks.push(task);
-      }
-    }
-  }
-
-  let finalAllTasks;
-  if (selectedDay) {
-    finalAllTasks = isSameDay(selectedDay, today) ? selectedDayTasks : allTasks;
-  } else {
-    finalAllTasks = selectedDayTasks;
-  }
+  const allTasks = selectedDayTasks;
+  const todoTasks = selectedDayTasks.filter(isTodo);
+  const inProgressTasks = selectedDayTasks.filter(isInProgress);
+  const overdueTasks = selectedDayTasks.filter(isOverdue);
 
   return [
     {
       status: "All",
-      count: finalAllTasks.length,
-      tasks: finalAllTasks,
+      count: allTasks.length,
+      tasks: allTasks,
     },
     {
       status: "To Do",
@@ -95,3 +53,15 @@ function isTodo(task: TaskDetailDTO): boolean {
 
   return true;
 }
+
+const isInProgress = (task: TaskDetailDTO) => {
+  if (!task.startTime || !task.endTime) return false;
+  const start = new Date(task.startTime);
+  const end = new Date(task.endTime);
+  return !task.isDone && start <= new Date() && end > new Date();
+};
+
+const isOverdue = (task: TaskDetailDTO) => {
+  if (!task.endTime) return false;
+  return !task.isDone && isBefore(new Date(task.endTime), new Date());
+};
