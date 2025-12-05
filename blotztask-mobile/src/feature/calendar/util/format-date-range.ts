3ba172b62b847051853433e0feb7df4bf0c17655
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-06 09:41:34 +1100

    fix task card date

--- blotztask-mobile/src/feature/calendar/util/format-date-range.ts
+++ blotztask-mobile/src/feature/calendar/util/format-date-range.ts
@@ -8,9 +8,11 @@ export const formatDateRange = ({
   endTime?: string;
 }) => {
   const formatToken = "dd/MM/yyyy";
+  const todayStr = format(new Date(), formatToken);
 
   if (startTime && endTime && startTime === endTime) {
-    return `${format(new Date(startTime), formatToken)}`;
+    const singleDate = format(new Date(startTime), formatToken);
+    return singleDate === todayStr ? "" : singleDate;
   } else if (startTime && endTime && startTime !== endTime) {
     return `${format(new Date(startTime), formatToken)} - ${format(new Date(endTime), formatToken)}`;
   } else if (!startTime || !endTime) {
