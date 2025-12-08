46ffe0444eeca5bbe5eadf3e9e937576356c06bd
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-08 11:45:06 +1100

    fix calendar

--- blotztask-mobile/src/feature/task-add-edit/components/single-date-calendar.tsx
+++ blotztask-mobile/src/feature/task-add-edit/components/single-date-calendar.tsx
@@ -3,11 +3,15 @@ import { useState } from "react";
 import { Calendar, DateData } from "react-native-calendars";
 
 export const SingleDateCalendar = ({
+  defaultStartDate,
   onStartDateChange,
 }: {
+  defaultStartDate: string;
   onStartDateChange: (...event: any[]) => void;
 }) => {
-  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [selectedDate, setSelectedDate] = useState<string | null>(defaultStartDate);
+  console.log("defaultStartDate", defaultStartDate);
+
   return (
     <Calendar
       onDayPress={(day: DateData) => {
