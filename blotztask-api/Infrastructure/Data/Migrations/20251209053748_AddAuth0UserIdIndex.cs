4512c310be82f5c1777dc4059ca10601de3d210b
Xining Nan <nicolenxn1234@gmail.com>
2025-12-09 18:04:35 +1100

    add AddAuth0UserIdIndex (#68)

---
+++ blotztask-api/Infrastructure/Data/Migrations/20251209053748_AddAuth0UserIdIndex.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlotzTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuth0UserIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Auth0UserId",
+                table: "AppUsers",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_Auth0UserId",
+                table: "AppUsers",
+                column: "Auth0UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppUsers_Auth0UserId",
+                table: "AppUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Auth0UserId",
+                table: "AppUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
