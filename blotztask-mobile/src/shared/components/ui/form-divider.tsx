dbe54a9f1d5d0dba63ef51d98a6dfa60ec0f6f10
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-11 17:40:31 +1100

    add form divider

--- blotztask-mobile/src/shared/components/ui/form-divider.tsx
+++ blotztask-mobile/src/shared/components/ui/form-divider.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import { View } from "react-native";
 
 export const FormDivider = () => {
-  return <View className="border-dashed border-gray-300 mb-8 border" />;
+  return (
+    <View
+      style={{
+        height: 1,
+        width: "100%",
+        borderRadius: 1,
+        borderWidth: 1,
+        borderColor: "#D1D5DB",
+        borderStyle: "dashed",
+        marginVertical: 16,
+      }}
+    >
+      <View
+        style={{
+          left: 0,
+          bottom: 0,
+          width: "100%",
+          height: 1,
+          backgroundColor: "white",
+        }}
+      />
+    </View>
+  );
 };
