ca015a983976152f8f19ea668df8e2f519e846d0
Xining Nan <nicolenxn1234@gmail.com>
2025-12-12 09:52:38 +1100

    set up update user name (#70)

--- blotztask-api/Modules/Users/DependencyInjection.cs
+++ blotztask-api/Modules/Users/DependencyInjection.cs
@@ -1,15 +1,25 @@
 using BlotzTask.Modules.Users.Commands;
+using BlotzTask.Modules.Users.Dtos;
 using BlotzTask.Modules.Users.Queries;
+using BlotzTask.Modules.Users.Services;
 
 namespace BlotzTask.Modules.Users;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddUserModule(this IServiceCollection services)
+    public static IServiceCollection AddUserModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        // Manual registration of command handlers
+        services.Configure<Auth0ManagementSettings>(options =>
+        {
+            configuration.GetSection("Auth0:Management").Bind(options);
+            options.Domain = configuration["Auth0:Domain"];
+        });
         services.AddScoped<SyncUserCommandHandler>();
         services.AddScoped<GetUserProfileQueryHandler>();
+        services.AddScoped<UpdateUserProfileCommandHandler>();
+        services.AddScoped<IAuth0ManagementService, Auth0ManagementService>();
         // Manual registration of query handlers 
         return services;
     }
