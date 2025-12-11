e4f1b96335f68e77c5ea4caef0c6d9ead583d8a0
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-11 17:54:06 +1100

    add dropdown

---
+++ blotztask-mobile/src/feature/task-add-edit/components/reminder-select.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native-reanimated/lib/typescript/Animated";
+
+export const ReminderSelect = () => {
+  return (
+    <View className="flex-row items-center">
+      <Text className="font-baloo text-secondary text-2xl mt-1">Reminder </Text>
+    </View>
+  );
+};
