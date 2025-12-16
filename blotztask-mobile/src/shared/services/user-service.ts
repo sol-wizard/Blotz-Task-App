719fc513037316aa15ea2dd9728ea0b83557fdfe
Xining Nan <nicolenxn1234@gmail.com>
2025-12-16 21:55:48 +1100

    Create frontend user name update (#83)
    Co-authored-by: Emilia-Z <keirinzeng@gmail.com>

--- blotztask-mobile/src/shared/services/user-service.ts
+++ blotztask-mobile/src/shared/services/user-service.ts
@@ -1,5 +1,6 @@
 import { UserProfileDTO } from "@/shared/models/user-profile-dto";
 import { apiClient } from "./api/client";
+import { UpdateUserProfileDTO } from "@/feature/settings/modals/update-user-profile-dto";
 
 export const fetchUserProfile = async (): Promise<UserProfileDTO> => {
   const url = `${process.env.EXPO_PUBLIC_URL_WITH_API}/User`;
@@ -8,5 +9,15 @@ export const fetchUserProfile = async (): Promise<UserProfileDTO> => {
   } catch (err: any) {
     console.error("Fetch user profile failed:", err);
     throw new Error("Failed to load user profile data");
+  }
+};
+
+export const updateUserProfile = async (userProfile: UpdateUserProfileDTO): Promise<string> => {
+  const url = `${process.env.EXPO_PUBLIC_URL_WITH_API}/User`;
+  try {
+    return await apiClient.put(url, userProfile);
+  } catch (err: any) {
+    console.error("Update user profile failed:", err);
+    throw new Error("Failed to update user profile data");
   }
 };
