4815458bc3603f2eab0fef71fd3de07691aa3f3b
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-16 16:10:43 +1100

    fix account page

--- blotztask-mobile/src/app/(protected)/settings/account.tsx
+++ blotztask-mobile/src/app/(protected)/settings/account.tsx
@@ -4,26 +4,80 @@ import { ReturnButton } from "@/shared/components/ui/return-button";
 import { useLogout } from "@/shared/hooks/uselogout";
 import { usePostHog } from "posthog-react-native";
 import { router } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FormDivider } from "@/shared/components/ui/form-divider";
+import { useUserProfile } from "@/shared/hooks/useUserProfile";
+import LoadingScreen from "@/shared/components/ui/loading-screen";
 
 export default function AccountScreen() {
   const logout = useLogout();
   const posthog = usePostHog();
+
+  const { userProfile, isUserProfileLoading } = useUserProfile();
+
   const handleSignOut = async () => {
     await logout();
     posthog.reset();
     router.replace("/(auth)/onboarding");
   };
+  if (isUserProfileLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
-    <SafeAreaView className="flex-1 bg-background items-center">
-      <View className="flex-row px-6 pt-6">
+    <SafeAreaView className="flex-1 bg-background items-center px-6">
+      <View className="flex-row pt-6">
         <ReturnButton />
         <View className="flex-1 items-center">
           <Text className="text-3xl font-balooExtraBold text-secondary">Account</Text>
         </View>
       </View>
+
+      <View className="mt-8 w-full bg-white rounded-2xl items-center">
+        <View className="w-11/12">
+          <Pressable className="px-4 ml-3">
+            <View className="flex-row items-center justify-between pt-4 pb">
+              <Text className="text-lg font-baloo text-secondary ">Name</Text>
+
+              <View className="flex-row items-center ">
+                <Text className="text-lg font-baloo text-secondary mr-2">
+                  {userProfile?.displayName}
+                </Text>
+                <MaterialCommunityIcons name="chevron-right" size={22} color="#444964" />
+              </View>
+            </View>
+            <Text className="text-primary font-balooThin text-sm">
+              This name appears across your account.
+            </Text>
+          </Pressable>
+
+          <FormDivider marginVertical={2} />
+          <Pressable className="px-4 ml-3">
+            <View className="flex-row items-center justify-between pt-4 pb">
+              <Text className="text-lg font-baloo text-secondary ">Email</Text>
+
+              <View className="flex-row items-center ">
+                <Text className="text-lg font-baloo text-secondary mr-2">{userProfile?.email}</Text>
+                <MaterialCommunityIcons name="chevron-right" size={22} color="#444964" />
+              </View>
+            </View>
+            <Text className="text-primary font-balooThin text-sm">
+              Used for login and notifications.
+            </Text>
+          </Pressable>
+
+          <FormDivider marginVertical={2} />
+          <Pressable className="flex-row items-center justify-between px-4 py-4">
+            <View className="flex-row items-center">
+              <Text className="text-lg font-baloo text-secondary ml-3">Password</Text>
+            </View>
+            <MaterialCommunityIcons name="chevron-right" size={22} color="#444964" />
+          </Pressable>
+        </View>
+      </View>
       <Pressable
         onPress={handleSignOut}
-        className="bg-white rounded-xl w-96 py-4 items-center justify-center pr-4 shadow"
+        className="bg-white rounded-xl w-96 py-4 items-center justify-center pr-4 shadow mt-12"
       >
         <Text className="text-red-500 font-baloo text-xl ml-4">Sign Out</Text>
       </Pressable>
