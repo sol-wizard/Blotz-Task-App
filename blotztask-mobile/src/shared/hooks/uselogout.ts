23380c4ad140f6fdfaf38df697fafd17f106a985
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-18 15:41:38 +1100

    fix auth0 logout

--- blotztask-mobile/src/shared/hooks/uselogout.ts
+++ blotztask-mobile/src/shared/hooks/uselogout.ts
@@ -1,16 +1,21 @@
-import { useRouter } from "expo-router";
 import * as SecureStore from "expo-secure-store";
-import { AUTH_TOKEN_KEY } from "@/shared/constants/token-key";
+import { useRouter } from "expo-router";
+import { useAuth0 } from "react-native-auth0";
 
 export function useLogout() {
   const router = useRouter();
 
   return async () => {
     try {
-      await SecureStore.deleteItemAsync(AUTH_TOKEN_KEY);
-      router.replace("/onboarding");
+      await Promise.allSettled([
+        SecureStore.deleteItemAsync("AUTH_TOKEN_KEY"),
+        SecureStore.deleteItemAsync("REFRESH_TOKEN_KEY"),
+      ]);
+      console.log("🎯 clear session successfully");
     } catch (e) {
-      console.log("Logout error:", e);
+      console.log("clearSession error:", e);
+    } finally {
+      router.replace("/onboarding");
     }
   };
 }
