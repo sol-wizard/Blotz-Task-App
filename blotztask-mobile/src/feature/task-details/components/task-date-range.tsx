8bc5c93d054a38ea296fe604a431b07e5d9f6a04
AlexisEvans <alexissong07@gmail.com>
2025-12-18 23:44:20 +1100

    try some change

--- blotztask-mobile/src/feature/task-details/components/task-date-range.tsx
+++ blotztask-mobile/src/feature/task-details/components/task-date-range.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
-import { format } from "date-fns";
+import { format, isToday, isTomorrow } from "date-fns";
 import { View, Text } from "react-native";
 
 const DateItem = ({ label, value }: { label: string; value?: string }) => {
-  const formatDate = (val?: string) => (val ? format(new Date(val), "dd/MM/yyyy hh:mm a") : "-");
+  const formatDate = (val?: string) => {
+    if (!val) return "-";
+
+    const d = new Date(val);
+
+    if (isToday(d)) return "Today";
+    if (isTomorrow(d)) return "Tomorrow";
+
+    return format(d, "dd/MM/yyyy hh:mm a");
+  };
+
   return (
     <View className="flex-row items-center mr-10">
       <View className="w-10 h-10 rounded-full border-2 border-gray-500 border-dashed justify-center items-center mr-2">
