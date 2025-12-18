75659261bc64bb9bad2ad3826c91a1b8ef03f760
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-18 17:43:51 +1100

    remove

--- blotztask-mobile/src/feature/star-spark/models/task-time-estimation.ts
+++ blotztask-mobile/src/feature/star-spark/models/task-time-estimation.ts
@@ -2,8 +2,3 @@ export interface TaskTimeEstimation {
   taskId: number;
   duration: string;
 }
-
-export interface EstimatedTaskTime extends TaskTimeEstimation {
-  durationText: string;
-  durationMinutes: number;
-}
