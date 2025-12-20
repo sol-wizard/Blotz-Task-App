7d2139c8a739a7ef3810b3b61f8e48480f2e66be
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-20 16:36:13 +1100

    update get preference frontend

---
+++ blotztask-mobile/src/feature/settings/hooks/useUserPreferencesQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchUserPreferences } from "@/shared/services/user-service";
+import { UserPreferencesDTO } from "@/shared/models/user-preferences-dto";
+
+export function useUserPreferencesQuery() {
+  const userPreferencesQuery = useQuery<UserPreferencesDTO>({
+    queryKey: ["userPreferences"],
+    queryFn: () => fetchUserPreferences(),
+  });
+
+  return {
+    fetchUserPreferencesAsync: userPreferencesQuery.refetch,
+    isUserPreferencesLoading: userPreferencesQuery.isLoading,
+    userPreferencesError: userPreferencesQuery.error,
+    userPreferences: userPreferencesQuery.data,
+  };
+}
--- blotztask-mobile/src/shared/services/user-service.ts
+++ blotztask-mobile/src/shared/services/user-service.ts
@@ -1,4 +1,5 @@
 import { UserProfileDTO } from "@/shared/models/user-profile-dto";
+import { UserPreferencesDTO } from "@/shared/models/user-preferences-dto";
 import { apiClient } from "./api/client";
 import { UpdateUserProfileDTO } from "@/feature/settings/modals/update-user-profile-dto";
 
@@ -19,5 +20,15 @@ export const updateUserProfile = async (userProfile: UpdateUserProfileDTO): Prom
   } catch (err: any) {
     console.error("Update user profile failed:", err);
     throw new Error("Failed to update user profile data");
+  }
+};
+
+export const fetchUserPreferences = async (): Promise<UserPreferencesDTO> => {
+  const url = `${process.env.EXPO_PUBLIC_URL_WITH_API}/user-preferences`;
+  try {
+    return await apiClient.get(url);
+  } catch (err: any) {
+    console.error("Fetch user preferences failed:", err);
+    throw new Error("Failed to load user preferences");
   }
 };
