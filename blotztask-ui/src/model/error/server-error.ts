f0490eb56b9350299be2e243ef47fd78a03b323b
sol-wizard <benjaminneoh2928@gmail.com>
2025-12-23 11:21:07 +1100

    remove UI project

--- blotztask-ui/src/model/error/server-error.ts
+++
@@ -1,9 +0,0 @@
-export class ServerError extends Error {
-  public status: number;
-
-  constructor(message: string, status: number) {
-    super(message);
-    this.name = 'ServerError';
-    this.status = status;
-  }
-}
