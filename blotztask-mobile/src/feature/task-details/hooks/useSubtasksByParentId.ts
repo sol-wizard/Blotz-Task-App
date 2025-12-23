da82a58e9ec1ae7eb9c61efa0da992b6f78caa5a
Xining Nan <nicolenxn1234@gmail.com>
2025-12-23 11:22:27 +1100

    Fix tan stack query client (#97)

--- blotztask-mobile/src/feature/task-details/hooks/useSubtasksByParentId.ts
+++ blotztask-mobile/src/feature/task-details/hooks/useSubtasksByParentId.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getSubtasksByParentId } from "@/feature/task-details/services/subtask-service";
 import { SubtaskDTO } from "@/feature/task-details/models/subtask-dto";
+import { subtaskKeys } from "@/shared/util/query-key-factory";
 
 export const useSubtasksByParentId = (parentId: number) => {
   return useQuery<SubtaskDTO[], Error>({
-    queryKey: ["subtasks", parentId],
+    queryKey: subtaskKeys.all(parentId),
     queryFn: () => getSubtasksByParentId(parentId),
     enabled: !!parentId,
   });
