811ee44ac548367de012aba3f175f942957c5bc7
Aug-8 <130132558+sol-wizard@users.noreply.github.com>
2025-12-23 11:34:32 +1100

    remove UI project (#98)
    Co-authored-by: Xining Nan <nicolenxn1234@gmail.com>

--- blotztask-ui/src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-//TODO: Made a shared loading component for the whole application - the same loading compoent is used in the daashboard layout
-export default function Loading() {
-    return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/80">
-        <div className="text-primary text-xl">Loading dashboard...</div>
-      </div>
-    );
-  }
--- blotztask-ui/src/model/add-task-item-dto.ts
+++
@@ -1,6 +0,0 @@
-export interface AddTaskItemDTO {
-  title: string;
-  description: string;
-  endTime: string;
-  labelId: number;
-}
