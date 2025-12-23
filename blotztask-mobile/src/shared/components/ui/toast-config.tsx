06b9e49a9f69166744851872db0c60c2d1cb0210
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-23 11:19:04 +1100

    change to icon

--- blotztask-mobile/src/shared/components/ui/toast-config.tsx
+++ blotztask-mobile/src/shared/components/ui/toast-config.tsx
@@ -1,9 +1,10 @@
-import { TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Toast, { ErrorToast } from "react-native-toast-message";
 
 export const toastConfig = {
   error: (props: any) => (
-    <View className="flex-row items-center bg-gray-900 rounded-[10px] h-10 px-3 w-96">
+    <View className="flex-row items-center bg-gray-900 rounded-xl h-10 px-3 w-96">
       <View className="flex-1">
         <ErrorToast
           {...props}
@@ -21,7 +22,7 @@ export const toastConfig = {
         onPress={() => Toast.hide()}
         hitSlop={{ top: 8, bottom: 8, left: 8, right: 8 }}
       >
-        <Text className="text-white text-base ml-2">✕</Text>
+        <MaterialCommunityIcons name="close" size={18} color="#fff" style={{ marginLeft: 8 }} />
       </TouchableOpacity>
     </View>
   ),
