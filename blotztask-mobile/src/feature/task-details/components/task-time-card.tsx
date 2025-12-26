22f568eefd9f9e436f223ee5862cf48b3a1397fa
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-26 14:41:28 +1100

    create

---
+++ blotztask-mobile/src/feature/task-details/components/task-time-card.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export const TaskSingleTimeCard = () => {
+  return <View></View>;
+};
+
+export const TaskRangeTimeCard = () => {
+  return <View></View>;
+};
