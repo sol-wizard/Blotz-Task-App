ee2d84ea0585e7ac9c374ec52b65d048e0554b79
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-26 14:51:52 +1100

    create format

--- blotztask-mobile/src/feature/task-details/components/task-time-card.tsx
+++ blotztask-mobile/src/feature/task-details/components/task-time-card.tsx
@@ -1,9 +1,15 @@
 import { View } from "react-native";
 
-export const TaskSingleTimeCard = () => {
+export const TaskSingleTimeCard = ({ startTime }: { startTime: string }) => {
   return <View></View>;
 };
 
-export const TaskRangeTimeCard = () => {
+export const TaskRangeTimeCard = ({
+  startTime,
+  endTime,
+}: {
+  startTime: string;
+  endTime: string;
+}) => {
   return <View></View>;
 };
