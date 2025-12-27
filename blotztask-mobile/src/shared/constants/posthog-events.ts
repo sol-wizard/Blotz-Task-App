30a83470b7ed84e323d7d672e70edfbf350ffea8
nicole-nxn <nicolenxn1234@gmail.com>
2025-12-27 19:08:05 +1100

    update posthog

--- blotztask-mobile/src/shared/constants/posthog-events.ts
+++ blotztask-mobile/src/shared/constants/posthog-events.ts
@@ -2,4 +2,5 @@ export const EVENTS = {
   CREATE_TASK_MANUALLY: "create_task_manually",
   CREATE_TASK_BY_AI: "create_task_by_ai",
   ACTIVE_USER_5S: "active_user_5s",
+  BREAKDOWN_TASK: "breakdown_task",
 };
