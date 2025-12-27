f507b4af978372672887e7c2bea84c84f7aa47a1
guotai <guotaixiao87@gmail.com>
2025-12-27 19:11:22 +1100

    fix: remov unused imports

--- blotztask-mobile/src/app/(protected)/ai-task-sheet.tsx
+++ blotztask-mobile/src/app/(protected)/ai-task-sheet.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { View, Pressable } from "react-native";
 import { router } from "expo-router";
 import { BottomSheetType } from "@/feature/ai-task-generate/models/bottom-sheet-type";
 import { AiModalContent } from "@/feature/ai-task-generate/component/ai-modal-content";
 import { KeyboardAvoidingView } from "react-native-keyboard-controller";
-import { installAndroidLanguagePackage } from "@/feature/ai-task-generate/utils/install-android-language-package";
-import { requestMicrophonePermission } from "@/feature/ai-task-generate/utils/request-microphone-permission";
-import { CloseButton } from "@/feature/ai-task-generate/component/close-button";
-import { AiLanguage } from "@/feature/ai-task-generate/component/ai-language";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function AiTaskSheetScreen() {
   const [modalType, setModalType] = useState<BottomSheetType>("input");
