f099fcc63d60a27b9f82125d0073351d14fc3fe7
LiChenyang-GZ <1169281314@qq.com>
2026-01-09 17:07:34 +1100

    fix Android Toast Shadow

--- blotztask-mobile/src/shared/components/ui/toast-config.tsx
+++ blotztask-mobile/src/shared/components/ui/toast-config.tsx
@@ -1,21 +1,28 @@
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Text, Platform } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import Toast, { ErrorToast } from "react-native-toast-message";
+import Toast from "react-native-toast-message";
 
 export const toastConfig = {
   error: (props: any) => (
-    <View className="flex-row items-center bg-gray-900 rounded-xl h-10 px-3 w-96">
-      <View className="flex-1">
-        <ErrorToast
-          {...props}
-          style={{ backgroundColor: "transparent", borderLeftWidth: 0, height: 40 }}
-          contentContainerStyle={{ paddingHorizontal: 0, paddingLeft: 8 }}
-          text1Style={{
+    <View
+      style={{
+        elevation: 0,
+        shadowOpacity: 0,
+      }}
+      className="flex-row items-center bg-gray-900 rounded-xl h-10 px-3 w-96"
+    >
+      <View className="flex-1 justify-center">
+        <Text
+          numberOfLines={1}
+          style={{
             color: "#fff",
             fontSize: 14,
             fontFamily: "BalooRegular",
+            paddingLeft: 8,
           }}
-        />
+        >
+          {props.text1}
+        </Text>
       </View>
 
       <TouchableOpacity
