a6000b475082f1607cec24e09d6057fbb61d1aa8
nicole-nxn <nicolenxn1234@gmail.com>
2026-01-28 09:54:07 +0800

    fix type any

--- blotztask-mobile/src/feature/gashapon-machine/components/dropped-star.tsx
+++ blotztask-mobile/src/feature/gashapon-machine/components/dropped-star.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Dimensions, StyleSheet, Image, Easing } from "react-native";
+import { Animated, Dimensions, StyleSheet, Image, Easing, ImageSourcePropType } from "react-native";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -9,7 +9,7 @@ const DROPOUT_Y = SCREEN_HEIGHT / 2 + 200;
 interface DroppedStarProps {
   trigger: number;
   setTaskRevealModalVisible: (v: boolean) => void;
-  imageSource: any;
+  imageSource: ImageSourcePropType;
 }
 
 export const DroppedStar: React.FC<DroppedStarProps> = ({
--- blotztask-mobile/src/feature/notes/utils/get-star-icon.ts
+++ blotztask-mobile/src/feature/notes/utils/get-star-icon.ts
@@ -1,4 +1,5 @@
 import { ASSETS } from "@/shared/constants/assets";
+import { ImageSourcePropType } from "react-native";
 
 const STAR_ICONS = [
   ASSETS.purpleStar,
@@ -8,7 +9,7 @@ const STAR_ICONS = [
   ASSETS.rainbowStar,
 ];
 
-export const getStarIconAsBefore = (seed: number) => {
+export const getStarIconAsBefore = (seed: number): ImageSourcePropType => {
   const normalizedSeed = Math.abs(seed) % STAR_ICONS.length;
   return STAR_ICONS[normalizedSeed];
 };
