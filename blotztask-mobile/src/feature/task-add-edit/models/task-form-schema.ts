ff60e8f86148a512436cc672b2b7a9402bf179de
ReynaSakura <dorating0829@gmail.com>
2026-02-11 21:15:34 +1100

    Remove .nullable() for time and date

--- blotztask-mobile/src/feature/task-add-edit/models/task-form-schema.ts
+++ blotztask-mobile/src/feature/task-add-edit/models/task-form-schema.ts
@@ -6,10 +6,10 @@ export const taskFormSchema = z
   .object({
     title: z.string().trim().min(1, "Title is required").max(80, "Max 80 chars"),
     description: z.union([z.string().max(1000, "Max 1000 chars"), z.literal("")]).nullable(),
-    startDate: z.date().nullable(),
-    startTime: z.date().nullable(),
-    endDate: z.date().nullable(),
-    endTime: z.date().nullable(),
+    startDate: z.date(),
+    startTime: z.date(),
+    endDate: z.date(),
+    endTime: z.date(),
     labelId: z.number().nullable(),
     alert: z.number().nullable(),
   })
