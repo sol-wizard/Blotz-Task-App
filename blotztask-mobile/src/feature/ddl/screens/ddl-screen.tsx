22de606c73d1d336d839365635cb13f13a102845
Yifan Cui <yifan.cui@students.mq.edu.au>
2026-03-07 11:37:39 +1100

    Add `SafeAreaView` and removed `absolute`

--- blotztask-mobile/src/feature/ddl/screens/ddl-screen.tsx
+++ blotztask-mobile/src/feature/ddl/screens/ddl-screen.tsx
@@ -1,14 +1,16 @@
 import { View, Text } from "react-native";
 import { ReturnButton } from "@/shared/components/ui/return-button";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function DdlScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-lg">DDL page</Text>
-
-      <View className="absolute top-20 left-4">
+    <SafeAreaView className="flex-1 bg-background">
+      <View className="flex-row px-6 pt-6">
         <ReturnButton />
       </View>
-    </View>
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-lg">DDL page</Text>
+      </View>
+    </SafeAreaView>
   );
 }
