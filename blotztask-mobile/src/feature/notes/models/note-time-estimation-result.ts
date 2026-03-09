67e81dd83131fdd56ef213fe26c102bd0758c925
Xining Nan <nicolenxn1234@gmail.com>
2026-03-09 14:52:28 +1100

    Fix time estimation bug (#232)

---
+++ blotztask-mobile/src/feature/notes/models/note-time-estimation-result.ts
@@ -0,0 +1,5 @@
+export interface NoteTimeEstimationResult {
+  isSuccess: boolean;
+  errorMessage?: string;
+  duration: string;
+}
--- blotztask-mobile/src/feature/notes/models/note-time-estimation.ts
+++
@@ -1,4 +0,0 @@
-export interface NoteTimeEstimation {
-  noteId: string;
-  duration: string;
-}
