43a7842e4105cab01c4a669567db327d44abbaa6
HoshinoIzumi <hoshinoitsumi@gmail.com>
2026-03-11 12:06:46 +1100

    fix: required field

--- blotztask-api/Modules/Tasks/Domain/Entities/TaskDeadline.cs
+++ blotztask-api/Modules/Tasks/Domain/Entities/TaskDeadline.cs
@@ -4,18 +4,17 @@ namespace BlotzTask.Modules.Tasks.Domain.Entities;
 
 public class TaskDeadline
 {
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
     public int TaskItemId { get; set; }
     
-    public DateTimeOffset DueAt { get; set; }
+    public required DateTimeOffset DueAt { get; set; }
     
     public DateTime CreatedAt { get; set; }
     
     public DateTime UpdatedAt { get; set; }
     
-    public bool IsPinned { get; set; }
+    public bool IsPinned { get; set; } = false;
     
-    public TaskItem TaskItem { get; set; } = null!;
+    public required TaskItem TaskItem { get; set; }
 }
