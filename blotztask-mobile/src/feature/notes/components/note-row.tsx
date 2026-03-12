6239ceb12088c3a5d5adae9e7173eab4fb4ecd0b
LiYing Chen <78844463+imlililili@users.noreply.github.com>
2026-03-12 23:06:16 +1100

    Update note-row.tsx

--- blotztask-mobile/src/feature/notes/components/note-row.tsx
+++ blotztask-mobile/src/feature/notes/components/note-row.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import Animated from "react-native-reanimated";
 import ReanimatedSwipeable, {
   SwipeableMethods,
