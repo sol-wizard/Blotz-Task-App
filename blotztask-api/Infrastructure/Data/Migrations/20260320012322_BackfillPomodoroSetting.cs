682923fcf7c72935b6f206b02fc8b8054bc0a7e0
LiChenyang-GZ <1169281314@qq.com>
2026-03-20 13:30:50 +1100

    default PomodoroSetting for both new/old users

---
+++ blotztask-api/Infrastructure/Data/Migrations/20260320012322_BackfillPomodoroSetting.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlotzTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillPomodoroSetting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            INSERT INTO [dbo].[PomodoroSettings] ([UserId], [Timing], [Sound], [IsCountdown])
+            SELECT u.[Id], 25, NULL, 0
+            FROM [dbo].[AppUsers] u
+            LEFT JOIN [dbo].[PomodoroSettings] p ON p.[UserId] = u.[Id]
+            WHERE p.[UserId] IS NULL;
+            """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
