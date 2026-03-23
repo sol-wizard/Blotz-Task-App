54db6a5df17b720474c06c8ae2570c8782aa3042
ReynaSakura <dorating0829@gmail.com>
2026-03-24 01:11:28 +1100

    Add a space in between time unit and number

--- blotztask-mobile/src/shared/util/convert-duration.ts
+++ blotztask-mobile/src/shared/util/convert-duration.ts
@@ -2,13 +2,13 @@ export function convertDurationToText(timeStr: string): string {
   const [hours, minutes, seconds] = timeStr.split(":").map(Number);
 
   if (minutes === 0 && seconds === 0) {
-    return `${hours}h`;
+    return `${hours} h`;
   } else if (hours === 0 && seconds === 0) {
-    return `${minutes}min`;
+    return `${minutes} min`;
   } else if (hours > 0 && minutes > 0 && seconds === 0) {
-    return `${minutes}min`;
+    return `${minutes} min`;
   } else {
-    return `${hours > 0 ? hours + "h " : ""}${minutes > 0 ? minutes + "min" : ""}`;
+    return `${hours > 0 ? hours + " h " : ""}${minutes > 0 ? minutes + " min" : ""}`;
   }
 }
 
