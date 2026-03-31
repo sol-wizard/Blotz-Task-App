61814b5850caf9377d8b3fec4f78f47e97b02636
guotai <guotaixiao87@gmail.com>
2026-03-31 16:39:17 +1100

    update: add LoginAt Column

---
+++ blotztask-api/Infrastructure/Data/Migrations/20260331053828_addLoginTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlotzTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class addLoginTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LoginAt",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("718ccb8f-ce52-4e51-8cfe-2a44cdca77d1"),
+                column: "LoginAt",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoginAt",
+                table: "AppUsers");
+        }
+    }
+}
