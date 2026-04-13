a98b02b5ecfb896151de09d35beb1339f491ca43
LiYing Chen <78844463+imlililili@users.noreply.github.com>
2026-04-14 01:28:41 +1000

    bug fix: hour and min > 0

--- blotztask-mobile/src/shared/util/convert-duration.ts
+++ blotztask-mobile/src/shared/util/convert-duration.ts
@@ -5,8 +5,6 @@ export function convertDurationToText(timeStr: string): string {
     return `${hours} h`;
   } else if (hours === 0 && seconds === 0) {
     return `${minutes} min`;
-  } else if (hours > 0 && minutes > 0 && seconds === 0) {
-    return `${minutes} min`;
   } else {
     return `${hours > 0 ? hours + " h " : ""}${minutes > 0 ? minutes + " min" : ""}`;
   }
