cca5e2849e773640fc8a38f2f901b2a0e3862c7b
ReynaSakura <dorating0829@gmail.com>
2026-04-24 16:43:10 +1000

    Update navigation bar UI issue with some bottomsheet drag action fix

--- blotztask-mobile/src/feature/monthly-calendar/components/day-detail-panel.tsx
+++ blotztask-mobile/src/feature/monthly-calendar/components/day-detail-panel.tsx
@@ -1,4 +1,5 @@
-import { View, Text, ScrollView, Dimensions, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
+import { BottomSheetScrollView } from "@gorhom/bottom-sheet"; 
 import { format, parseISO } from "date-fns";
 import useSelectedDayTasks from "@/shared/hooks/useSelectedDayTasks";
 import { theme } from "@/shared/constants/theme";
@@ -12,10 +13,10 @@ export const SelectedDayDetailPanel = ({ selectedDay }: { selectedDay: Date }) =
       {isLoading ? (
         <ActivityIndicator size="large" color={theme.colors.onSurface} className="mt-10" />
       ) : (
-        <ScrollView
+        <BottomSheetScrollView
           showsVerticalScrollIndicator={false}
           className="flex-1"
-          contentContainerStyle={{ paddingBottom: Dimensions.get("window").height * 0.4 }}
+          contentContainerStyle={{ paddingBottom: 40 }}
         >
           {selectedDayTasks.length > 0 ? (
             selectedDayTasks.map((task, index) => {
@@ -70,7 +71,7 @@ export const SelectedDayDetailPanel = ({ selectedDay }: { selectedDay: Date }) =
               {i18n.t("calendar:emptyState.all.title")}
             </Text>
           )}
-        </ScrollView>
+        </BottomSheetScrollView>
       )}
     </View>
   );
