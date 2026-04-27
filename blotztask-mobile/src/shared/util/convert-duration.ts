459555a785c3942f5ecb93d2f265884d541f670a
LiYing Chen <78844463+imlililili@users.noreply.github.com>
2026-04-27 14:42:11 +1000

    Update convert-duration.ts

--- blotztask-mobile/src/shared/util/convert-duration.ts
+++ blotztask-mobile/src/shared/util/convert-duration.ts
@@ -5,8 +5,6 @@ export function convertDurationToText(timeStr: string): string {
     return `${minutes} min`;
   } else if (minutes === 0 && seconds === 0) {
     return `${hours} h`;
-  } else if (hours === 0 && seconds === 0) {
-    return `${minutes} min`;
   } else {
     return `${hours > 0 ? hours + " h " : ""}${minutes > 0 ? minutes + " min" : ""}`;
   }
