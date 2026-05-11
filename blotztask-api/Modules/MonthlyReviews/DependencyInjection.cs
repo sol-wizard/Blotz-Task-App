c78b8de85702a68dc48e9fd4e0fc41947d4001d3
sol-wizard <benjaminneoh2928@gmail.com>
2026-05-11 18:34:58 +1000

    ai service for montly report

--- blotztask-api/Modules/MonthlyReviews/DependencyInjection.cs
+++ blotztask-api/Modules/MonthlyReviews/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using BlotzTask.Modules.MonthlyReviews.Queries;
+using BlotzTask.Modules.MonthlyReviews.Services;
 
 namespace BlotzTask.Modules.MonthlyReviews;
 
@@ -7,6 +8,7 @@ public static class DependencyInjection
     public static IServiceCollection AddMonthlyReviewModule(this IServiceCollection services)
     {
         services.AddScoped<GetMonthlyReviewTaskSnapshotQueryHandler>();
+        services.AddScoped<IMonthlyReviewAiService, MonthlyReviewAiService>();
         return services;
     }
 }
