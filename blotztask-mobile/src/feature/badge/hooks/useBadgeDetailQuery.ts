8cada2b86c9511c339f65652ac56d4ffdc96b203
LiliZ3681 <137756343+LiliZ3681@users.noreply.github.com>
2026-07-01 22:43:58 +1000

    Revert "updated the querykey to resolve cache issue when updating language setting."
    This reverts commit 7fef4f17cc66d25bf62c53ff8dc636625036d910.

--- blotztask-mobile/src/feature/badge/hooks/useBadgeDetailQuery.ts
+++ blotztask-mobile/src/feature/badge/hooks/useBadgeDetailQuery.ts
@@ -1,13 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { badgeKeys } from "@/shared/constants/query-key-factory";
 import { fetchBadgeDetailById } from "../services/badge-service";
-import { useTranslation } from "react-i18next";
 
 export const useBadgeDetailQuery = (badgeId: number | null) => {
-  const { i18n } = useTranslation();
-
   const badgeDetailQuery = useQuery({
-    queryKey: [...badgeKeys.detail(badgeId ?? 0), i18n.language],
+    queryKey: badgeKeys.detail(badgeId ?? 0),
     queryFn: () => fetchBadgeDetailById(badgeId!),
     enabled: badgeId != null,
   });
--- blotztask-mobile/src/feature/badge/hooks/useBadgesQuery.ts
+++ blotztask-mobile/src/feature/badge/hooks/useBadgesQuery.ts
@@ -1,13 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { badgeKeys } from "@/shared/constants/query-key-factory";
 import { fetchAllBadges } from "../services/badge-service";
-import { useTranslation } from "react-i18next";
 
 export const useBadgesQuery = () => {
-  const { i18n } = useTranslation();
-
   const { data: badges, isLoading } = useQuery({
-    queryKey: [...badgeKeys.all, i18n.language],
+    queryKey: badgeKeys.all,
     queryFn: () => fetchAllBadges(),
   });
 
