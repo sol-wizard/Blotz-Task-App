d1a6d8f9b0e51e7aaf16d3737a44ead667e14e6d
Chen <cxu498148@gmail.com>
2026-07-19 16:17:41 +0800

    fix: add breakdown for recurring task in task detail page (#474)
    Co-authored-by: Aug-8 <130132558+sol-wizard@users.noreply.github.com>

--- blotztask-api/Modules/BreakDown/DependencyInjection.cs
+++ blotztask-api/Modules/BreakDown/DependencyInjection.cs
@@ -7,6 +7,7 @@ public static class DependencyInjection
     public static IServiceCollection AddTaskBreakdownModule(this IServiceCollection services)
     {
         services.AddScoped<BreakdownTaskCommandHandler>();
+        services.AddScoped<BreakdownAndReplaceTaskCommandHandler>();
 
         return services;
     }
