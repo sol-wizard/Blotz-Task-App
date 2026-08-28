34d6c1dd0a96f9dbb67ecb5efedf8645d2d90bed
nicole-nxn <nicolenxn1234@gmail.com>
2026-08-28 14:15:28 +1000

    add sentry span tracing

--- blotztask-mobile/src/shared/services/api/client.ts
+++ blotztask-mobile/src/shared/services/api/client.ts
@@ -1,6 +1,20 @@
 import axios, { AxiosInstance } from "axios";
+import * as Sentry from "@sentry/react-native";
 import { API_CONFIG } from "./config";
 import { setupRequestInterceptor, setupResponseInterceptor } from "./interceptors";
+
+// trace axios request with Sentry span to record request time
+function traced<T>(method: string, url: string, run: () => Promise<T>): Promise<T> {
+  return Sentry.startSpan(
+    {
+      // create fix path name
+      name: `${method} ${url.split("?")[0].replace(/\/\d+/g, "/:param")}`,
+      op: "http.client",
+      attributes: { "http.request.method": method, "url.full": url },
+    },
+    run,
+  );
+}
 
 function createApiClient() {
   const instance: AxiosInstance = axios.create(API_CONFIG);
@@ -10,32 +24,23 @@ function createApiClient() {
 
   return {
     async get<T>(url: string, config = {}): Promise<T> {
-      const response = await instance.get<T>(url, config);
-      return response.data;
+      return traced("GET", url, async () => (await instance.get<T>(url, config)).data);
     },
 
     async post<T>(url: string, data?: unknown, config = {}): Promise<T> {
-      const response = await instance.post<T>(url, data, config);
-      return response.data;
+      return traced("POST", url, async () => (await instance.post<T>(url, data, config)).data);
     },
 
     async put<T>(url: string, data?: unknown, config = {}): Promise<T> {
-      const response = await instance.put<T>(url, data, config);
-      return response.data;
+      return traced("PUT", url, async () => (await instance.put<T>(url, data, config)).data);
     },
 
     async patch<T>(url: string, data?: unknown, config = {}): Promise<T> {
-      const response = await instance.patch<T>(url, data, config);
-      return response.data;
+      return traced("PATCH", url, async () => (await instance.patch<T>(url, data, config)).data);
     },
 
     async delete<T>(url: string, config = {}): Promise<T> {
-      const response = await instance.delete<T>(url, config);
-      return response.data;
-    },
-
-    getRawInstance(): AxiosInstance {
-      return instance;
+      return traced("DELETE", url, async () => (await instance.delete<T>(url, config)).data);
     },
   };
 }
