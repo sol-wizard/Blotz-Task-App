242ba1441e16355ee3da43a602c4c04ea9cd7d63
FFFLAREEE <116449053+FFFLAREEE@users.noreply.github.com>
2026-09-26 21:40:57 +1000

    fix account value alignment

--- blotztask-mobile/src/feature/settings/screens/settings-account-screen.tsx
+++ blotztask-mobile/src/feature/settings/screens/settings-account-screen.tsx
@@ -91,6 +91,7 @@ export default function SettingsAccountScreen() {
               >
                 {displayEmail}
               </Text>
+              <View className="w-[22px]" />
             </View>
           </View>
           <Text className="text-primary font-balooThin text-sm">
